323d402e7d852da7ab08f0c8578c13e80accf818
andrea putzu <andrea.putzu@pagopa.it>
2025-03-19 12:20:30 +0100

    chore: adding gh action run release infra

--- infra/cert/01_key_vault_operations.tf
+++ infra/cert/01_key_vault_operations.tf
@@ -32,7 +32,7 @@ resource "null_resource" "upload_jwks" {
               pip install --require-hashes --requirement "${path.module}/utils/py/requirements.txt"
               az storage blob download \
                 --container-name '$web' \
-                --account-name "${replace(local.project, "-", "")}checkoutsa" \
+                --account-name "${replace(local.project_full, "-", "")}checkoutsa" \
                 --account-key ${data.azurerm_key_vault_secret.web_storage_access_key.value} \
                 --file "${path.module}/.terraform/tmp/oldjwks-test.json" \
                 --name '.well-known/jwks.json'
@@ -43,15 +43,15 @@ resource "null_resource" "upload_jwks" {
               fi
               az storage blob upload \
                 --container-name '$web' \
-                --account-name "${replace(local.project, "-", "")}checkoutsa" \
+                --account-name "${replace(local.project_full, "-", "")}checkoutsa" \
                 --account-key ${data.azurerm_key_vault_secret.web_storage_access_key.value} \
                 --file "${path.module}/.terraform/tmp/jwks-test.json" \
                 --overwrite true \
                 --name '.well-known/jwks-test.json'
               az cdn endpoint purge \
                 --resource-group ${data.azurerm_resource_group.checkout_fe_rg.name} \
-                --name "${local.project}-checkout-cdn-endpoint" \
-                --profile-name "${local.project}-checkout-cdn-profile" \
+                --name "${local.project_full}-checkout-cdn-endpoint" \
+                --profile-name "${local.project_full}-checkout-cdn-profile" \
                 --content-paths "/.well-known/jwks-test.json" \
                 --no-wait
           EOT
