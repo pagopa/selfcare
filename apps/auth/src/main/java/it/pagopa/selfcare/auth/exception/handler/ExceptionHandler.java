98a930bbdf723685658616f88856663416d7b597
AleDore <alessio85.dore@gmail.com>
2025-04-03 17:53:47 +0200

    Revert "fix claims name and internal error code"
    This reverts commit 1e53566c5aafb19beddec928ad482fe21efc59a1.

--- apps/auth/src/main/java/it/pagopa/selfcare/auth/exception/handler/ExceptionHandler.java
+++ apps/auth/src/main/java/it/pagopa/selfcare/auth/exception/handler/ExceptionHandler.java
@@ -49,6 +49,7 @@ public class ExceptionHandler {
     @ServerExceptionMapper
     public Response toResponse(InternalException exception) {
         LOGGER.error(PREFIX_LOGGER, SOMETHING_HAS_GONE_WRONG_IN_THE_SERVER, exception.getMessage());
-        return toResponse(exception);
+        Problem problem = new Problem(exception.getMessage(), null,  HttpStatus.SC_FORBIDDEN, exception.getMessage(), null);
+        return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(problem).build();
     }
 }
