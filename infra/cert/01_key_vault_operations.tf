ba4f4dca3bad227b911af675813f5e0642ec5476
andrea putzu <andrea.putzu@pagopa.it>
2025-05-27 13:03:16 +0200

    chore: Update v4

--- infra/cert/01_key_vault_operations.tf
+++ infra/cert/01_key_vault_operations.tf
@@ -1,8 +1,8 @@
 # JWT
 module "jwt" {
-  source = "github.com/pagopa/terraform-azurerm-v3.git//jwt_keys?ref=v8.86.0"
+  source = "github.com/pagopa/terraform-azurerm-v4.git//jwt_keys?ref=v6.8.0"
 
-  jwt_name            = "jwt-test"
+  jwt_name            = "jwt"
   key_vault_id        = data.azurerm_key_vault.key_vault.id
   cert_common_name    = "apim"
   cert_password       = ""
@@ -11,9 +11,9 @@ module "jwt" {
 }
 
 module "jwt_exchange" {
-  source = "github.com/pagopa/terraform-azurerm-v3.git//jwt_keys?ref=v8.86.0"
+  source = "github.com/pagopa/terraform-azurerm-v4.git//jwt_keys?ref=v6.8.0"
 
-  jwt_name            = "jwt-exchange-test"
+  jwt_name            = "jwt-exchange"
   key_vault_id        = data.azurerm_key_vault.key_vault.id
   cert_common_name    = "selfcare.pagopa.it"
   cert_password       = ""
@@ -34,9 +34,9 @@ resource "null_resource" "upload_jwks" {
                 --container-name '$web' \
                 --account-name "${replace(local.project_full, "-", "")}checkoutsa" \
                 --account-key ${data.azurerm_key_vault_secret.web_storage_access_key.value} \
-                --file "${path.module}/.terraform/tmp/oldjwks-test.json" \
+                --file "${path.module}/.terraform/tmp/oldjwks.json" \
                 --name '.well-known/jwks.json'
-              python "${path.module}/utils/py/jwksFromPems.py" "${path.module}/.terraform/tmp/oldjwks-test.json" "${module.jwt.jwt_kid}" "${module.jwt.certificate_data_pem}" "${module.jwt_exchange.jwt_kid}" "${module.jwt_exchange.certificate_data_pem}" > "${path.module}/.terraform/tmp/jwks-test.json"
+              python "${path.module}/utils/py/jwksFromPems.py" "${path.module}/.terraform/tmp/oldjwks.json" "${module.jwt.jwt_kid}" "${module.jwt.certificate_data_pem}" "${module.jwt_exchange.jwt_kid}" "${module.jwt_exchange.certificate_data_pem}" > "${path.module}/.terraform/tmp/jwks.json"
               if [ $? -eq 1 ]
               then
                 exit 1
@@ -45,14 +45,14 @@ resource "null_resource" "upload_jwks" {
                 --container-name '$web' \
                 --account-name "${replace(local.project_full, "-", "")}checkoutsa" \
                 --account-key ${data.azurerm_key_vault_secret.web_storage_access_key.value} \
-                --file "${path.module}/.terraform/tmp/jwks-test.json" \
+                --file "${path.module}/.terraform/tmp/jwks.json" \
                 --overwrite true \
-                --name '.well-known/jwks-test.json'
+                --name '.well-known/jwks.json'
               az cdn endpoint purge \
                 --resource-group ${data.azurerm_resource_group.checkout_fe_rg.name} \
                 --name "${local.project_full}-checkout-cdn-endpoint" \
                 --profile-name "${local.project_full}-checkout-cdn-profile" \
-                --content-paths "/.well-known/jwks-test.json" \
+                --content-paths "/.well-known/jwks.json" \
                 --no-wait
           EOT
   }
