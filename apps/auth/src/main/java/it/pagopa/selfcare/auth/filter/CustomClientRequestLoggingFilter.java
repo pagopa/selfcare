c75830d986c745134abdaf4339a24362200ef4a8
AleDore <alessio85.dore@gmail.com>
2025-06-25 11:34:17 +0200

    add full request logging

--- apps/auth/src/main/java/it/pagopa/selfcare/auth/filter/CustomClientRequestLoggingFilter.java
+++ apps/auth/src/main/java/it/pagopa/selfcare/auth/filter/CustomClientRequestLoggingFilter.java
@@ -1,6 +1,5 @@
 package it.pagopa.selfcare.auth.filter;
 
-
 import jakarta.ws.rs.client.ClientRequestContext;
 import jakarta.ws.rs.ext.Provider;
 import lombok.extern.slf4j.Slf4j;
@@ -8,22 +7,35 @@ import org.jboss.resteasy.reactive.client.spi.ResteasyReactiveClientRequestConte
 import org.jboss.resteasy.reactive.client.spi.ResteasyReactiveClientRequestFilter;
 
 import java.io.IOException;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Provider
 @Slf4j
 public class CustomClientRequestLoggingFilter implements ResteasyReactiveClientRequestFilter {
 
-    @Override
-    public void filter(ClientRequestContext requestContext) throws IOException {
-        ResteasyReactiveClientRequestFilter.super.filter(requestContext);
-    }
+  @Override
+  public void filter(ClientRequestContext requestContext) throws IOException {
+    ResteasyReactiveClientRequestFilter.super.filter(requestContext);
+  }
 
-    @Override
-    public void filter(ResteasyReactiveClientRequestContext requestContext) {
-        String endpoint = requestContext.getUri().getPath();
-        String query = requestContext.getUri().getQuery();
-        String method = requestContext.getMethod();
-        MDCUtils.addOperationIdAndParameters(method);
-        log.info("Request: method: {}, endpoint: {}, query: {}", method, endpoint, query);
-    }
+  @Override
+  public void filter(ResteasyReactiveClientRequestContext requestContext) {
+    String endpoint = requestContext.getUri().getPath();
+    String query = requestContext.getUri().getQuery();
+    String method = requestContext.getMethod();
+    MDCUtils.addOperationIdAndParameters(method);
+
+    String headers =
+        requestContext.getStringHeaders().entrySet().stream()
+            .map(
+                entry ->
+                    String.format(
+                        "Header name:%s - value:%s",
+                        entry.getKey(), String.join(",", entry.getValue())))
+            .collect(Collectors.joining(";"));
+    String body = requestContext.getEntity().toString();
+
+      log.info("Request: method: {}, endpoint: {}, headers; {}, query: {}, body: {}", method, endpoint, headers, query, body);
+  }
 }
