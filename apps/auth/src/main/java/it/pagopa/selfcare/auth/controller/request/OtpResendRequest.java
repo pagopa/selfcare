0af3a299931ac54ea402e904b7a160492db4c639
AleDore <alessio85.dore@gmail.com>
2025-07-02 15:25:55 +0200

    [#SELC-7302] - Add Resend OTP Api

---
+++ apps/auth/src/main/java/it/pagopa/selfcare/auth/controller/request/OtpResendRequest.java
@@ -0,0 +1,13 @@
+package it.pagopa.selfcare.auth.controller.request;
+
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+@Data
+public class OtpResendRequest {
+
+    @NotEmpty(message = "otpUuid is required")
+    @NotNull(message = "otpUuid is required")
+    private String otpUuid;
+}
