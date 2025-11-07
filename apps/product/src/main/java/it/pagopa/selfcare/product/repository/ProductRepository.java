b62804e58011d68397bd2e024bff930e0d639479
Giampiero Ferrara <giampiero.ferara@emeal.nttdata.com>
2025-11-07 14:23:08 +0100

    Added ProductRepository

---
+++ apps/product/src/main/java/it/pagopa/selfcare/product/repository/ProductRepository.java
@@ -0,0 +1,8 @@
+package it.pagopa.selfcare.product.repository;
+
+import io.quarkus.mongodb.panache.reactive.ReactivePanacheMongoRepositoryBase;
+import it.pagopa.selfcare.product.model.Product;
+import jakarta.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+public class ProductRepository implements ReactivePanacheMongoRepositoryBase<Product, String> { }
