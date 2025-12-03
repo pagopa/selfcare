c192ea62624a6157a2e7e90a662631ea68975657
andrea-putzu <106688558+andrea-putzu@users.noreply.github.com>
2025-12-03 17:44:50 +0100

    [SELC-8044] Product-CDC Initial commit

--- apps/product-cdc/src/main/java/it/pagopa/selfcare/product/mapper/ProductMapper.java
+++ apps/product-cdc/src/main/java/it/pagopa/selfcare/product/mapper/ProductMapper.java
@@ -1,16 +1,18 @@
 package it.pagopa.selfcare.product.mapper;
 
+import it.pagopa.selfcare.onboarding.common.InstitutionType;
+import it.pagopa.selfcare.onboarding.common.OnboardingStatus;
 import it.pagopa.selfcare.onboarding.common.PartyRole;
+import it.pagopa.selfcare.onboarding.common.WorkflowType;
+import it.pagopa.selfcare.product.entity.ProductRoleInfo;
+import it.pagopa.selfcare.product.entity.ProductStatus;
 import it.pagopa.selfcare.product.model.*;
 import it.pagopa.selfcare.product.model.enums.OnboardingType;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.Named;
 
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
 
 @Mapper(componentModel = "cdi")
@@ -46,15 +48,15 @@ public interface ProductMapper {
   it.pagopa.selfcare.product.entity.Product toResource(Product entity);
 
   @Named("mapProductStatus")
-  default it.pagopa.selfcare.product.entity.ProductStatus mapProductStatus(it.pagopa.selfcare.product.model.enums.ProductStatus status) {
+  default ProductStatus mapProductStatus(it.pagopa.selfcare.product.model.enums.ProductStatus status) {
     if (status == null) {
       return null;
     }
     return switch (status) {
-      case ACTIVE -> it.pagopa.selfcare.product.entity.ProductStatus.ACTIVE;
-      case INACTIVE -> it.pagopa.selfcare.product.entity.ProductStatus.INACTIVE;
-      case TESTING -> it.pagopa.selfcare.product.entity.ProductStatus.TESTING;
-      default -> it.pagopa.selfcare.product.entity.ProductStatus.PHASE_OUT;
+      case ACTIVE -> ProductStatus.ACTIVE;
+      case INACTIVE -> ProductStatus.INACTIVE;
+      case TESTING -> ProductStatus.TESTING;
+      default -> ProductStatus.PHASE_OUT;
     };
   }
 
@@ -62,34 +64,68 @@ public interface ProductMapper {
   it.pagopa.selfcare.product.entity.BackOfficeConfigurations toBackOfficeResource(BackOfficeEnvironmentConfiguration entity);
 
   @Mapping(target = "roles", source = "backOfficeRoles", qualifiedByName = "mapRoleResource")
-  it.pagopa.selfcare.product.entity.ProductRoleInfo toRoleResource(RoleMapping entity);
+  ProductRoleInfo toRoleResource(RoleMapping entity);
 
   @Named("mapRoleResource")
   it.pagopa.selfcare.product.entity.ProductRole toRoleResource(BackOfficeRole roles);
 
   @Mapping(target = "institutionType", source = "institutionType", qualifiedByName = "mapInstitutionType")
-  it.pagopa.selfcare.product.entity.OriginEntry toOriginEntryResource(it.pagopa.selfcare.product.model.OriginEntry entity);
+  it.pagopa.selfcare.product.entity.OriginEntry toOriginEntryResource(OriginEntry entity);
 
   @Named("mapInstitutionType")
-  default it.pagopa.selfcare.onboarding.common.InstitutionType mapInstitutionType(it.pagopa.selfcare.product.model.enums.InstitutionType institutionType) {
+  default InstitutionType mapInstitutionType(it.pagopa.selfcare.product.model.enums.InstitutionType institutionType) {
     return switch (institutionType) {
-      case PA -> it.pagopa.selfcare.onboarding.common.InstitutionType.PA;
-      case GSP -> it.pagopa.selfcare.onboarding.common.InstitutionType.GSP;
-      case PG -> it.pagopa.selfcare.onboarding.common.InstitutionType.PG;
-      case SA -> it.pagopa.selfcare.onboarding.common.InstitutionType.SA;
-      case PT -> it.pagopa.selfcare.onboarding.common.InstitutionType.PT;
-      case SCP -> it.pagopa.selfcare.onboarding.common.InstitutionType.SCP;
-      case PSP -> it.pagopa.selfcare.onboarding.common.InstitutionType.PSP;
-      case AS -> it.pagopa.selfcare.onboarding.common.InstitutionType.AS;
-      case REC -> it.pagopa.selfcare.onboarding.common.InstitutionType.REC;
-      case CON -> it.pagopa.selfcare.onboarding.common.InstitutionType.CON;
-      case PRV -> it.pagopa.selfcare.onboarding.common.InstitutionType.PRV;
-      case PRV_PF -> it.pagopa.selfcare.onboarding.common.InstitutionType.PRV_PF;
-      case GPU -> it.pagopa.selfcare.onboarding.common.InstitutionType.GPU;
-      case SCEC -> it.pagopa.selfcare.onboarding.common.InstitutionType.SCEC;
+      case PA -> InstitutionType.PA;
+      case GSP -> InstitutionType.GSP;
+      case PG -> InstitutionType.PG;
+      case SA -> InstitutionType.SA;
+      case PT -> InstitutionType.PT;
+      case SCP -> InstitutionType.SCP;
+      case PSP -> InstitutionType.PSP;
+      case AS -> InstitutionType.AS;
+      case REC -> InstitutionType.REC;
+      case CON -> InstitutionType.CON;
+      case PRV -> InstitutionType.PRV;
+      case PRV_PF -> InstitutionType.PRV_PF;
+      case GPU -> InstitutionType.GPU;
+      case SCEC -> InstitutionType.SCEC;
       default -> null;
     };
 
+  }
+
+  @Named("mapOnboardingStatus")
+  default OnboardingStatus mapInstitutionType(String onboardingStatus) {
+    return switch (onboardingStatus) {
+      case "TOBEVALIDATED" -> OnboardingStatus.TOBEVALIDATED;
+      case "PENDING" -> OnboardingStatus.PENDING;
+      case "COMPLETED" -> OnboardingStatus.COMPLETED;
+      case "FAILED" -> OnboardingStatus.FAILED;
+      case "REJECTED" -> OnboardingStatus.REJECTED;
+      case "DELETED" -> OnboardingStatus.DELETED;
+      default -> OnboardingStatus.REQUEST;
+    };
+  }
+
+  @Named("mapWorkflowType")
+  default WorkflowType mapInstitutionType(it.pagopa.selfcare.product.model.enums.WorkflowType workflowType) {
+    return switch (workflowType) {
+      case CONTRACT_REGISTRATION -> WorkflowType.CONTRACT_REGISTRATION;
+      case FOR_APPROVE -> WorkflowType.FOR_APPROVE;
+      case FOR_APPROVE_PT -> WorkflowType.FOR_APPROVE_PT;
+      case FOR_APPROVE_GPU -> WorkflowType.FOR_APPROVE_GPU;
+      case CONFIRMATION -> WorkflowType.CONFIRMATION;
+      case USERS -> WorkflowType.USERS;
+      case USERS_IMPORT -> WorkflowType.USERS_IMPORT;
+      case IMPORT -> WorkflowType.IMPORT;
+      case IMPORT_AGGREGATION -> WorkflowType.IMPORT_AGGREGATION;
+      case CONTRACT_REGISTRATION_AGGREGATOR -> WorkflowType.CONTRACT_REGISTRATION_AGGREGATOR;
+      case CONFIRMATION_AGGREGATE -> WorkflowType.CONFIRMATION_AGGREGATE;
+      case INCREMENT_REGISTRATION_AGGREGATOR -> WorkflowType.INCREMENT_REGISTRATION_AGGREGATOR;
+      case CONFIRMATION_AGGREGATOR -> WorkflowType.CONFIRMATION_AGGREGATOR;
+      case USERS_PG -> WorkflowType.USERS_PG;
+      case USERS_EA -> WorkflowType.USERS_EA;
+    };
   }
 
   @Mapping(target = "contractTemplatePath", source = "path")
@@ -100,6 +136,8 @@ public interface ProductMapper {
 
   @Mapping(target = "templatePath", source = "path")
   @Mapping(target = "templateVersion", source = "version")
+  @Mapping(target = "workflowType", source = "workflowType", qualifiedByName = "mapWorkflowType")
+  @Mapping(target = "workflowState", source = "workflowState", qualifiedByName = "mapOnboardingStatus")
   it.pagopa.selfcare.product.entity.AttachmentTemplate toAttachmentResource(ContractTemplate entity);
 
   @Named("mapBackOfficeConfigsProdurlBOurl")
@@ -133,7 +171,7 @@ public interface ProductMapper {
   }
 
   @Named("mapRoleMappings")
-  default Map<PartyRole, it.pagopa.selfcare.product.entity.ProductRoleInfo> mapRoleMappings(List<RoleMapping> list) {
+  default Map<PartyRole, ProductRoleInfo> mapRoleMappings(List<RoleMapping> list) {
     if (list == null) {
       return null;
     }
@@ -141,26 +179,26 @@ public interface ProductMapper {
   }
 
   @Named("mapEmailTemplates")
-  default Map<String, Map<String, List<it.pagopa.selfcare.product.entity.EmailTemplate>>> mapEmailTemplates(List<it.pagopa.selfcare.product.model.EmailTemplate> emailTemplates) {
+  default Map<String, Map<String, List<it.pagopa.selfcare.product.entity.EmailTemplate>>> mapEmailTemplates(List<EmailTemplate> emailTemplates) {
     if (emailTemplates == null) {
       return Collections.emptyMap();
     }
     
     Map<String, Map<String, List<it.pagopa.selfcare.product.entity.EmailTemplate>>> result = new HashMap<>();
     
-    for (it.pagopa.selfcare.product.model.EmailTemplate template : emailTemplates) {
+    for (EmailTemplate template : emailTemplates) {
       String institutionType = template.getInstitutionType() != null ? template.getInstitutionType().name() : "default";
       String workflowType = template.getType() != null ? template.getType().name() : "default";
 
       result.computeIfAbsent(institutionType, k -> new HashMap<>())
-            .computeIfAbsent(workflowType, k -> new java.util.ArrayList<>())
+            .computeIfAbsent(workflowType, k -> new ArrayList<>())
             .add(toEmailTemplateResource(template));
     }
     
     return result;
   }
 
-  it.pagopa.selfcare.product.entity.EmailTemplate toEmailTemplateResource(it.pagopa.selfcare.product.model.EmailTemplate template);
+  it.pagopa.selfcare.product.entity.EmailTemplate toEmailTemplateResource(EmailTemplate template);
 
 
   default Map<String, it.pagopa.selfcare.product.entity.ContractTemplate> mapContracts(List<ContractTemplate> contracts, OnboardingType type) {
