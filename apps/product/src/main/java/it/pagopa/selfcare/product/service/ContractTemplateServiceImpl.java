cffd20f2bfb0b597c192a1917d5a213b49b68bff
Gaetano Miglionico <gaetano.miglionico@pagopa.it>
2025-12-05 15:35:36 +0100

    [SELC-8046] Adding product logo url to contract template

--- apps/product/src/main/java/it/pagopa/selfcare/product/service/ContractTemplateServiceImpl.java
+++ apps/product/src/main/java/it/pagopa/selfcare/product/service/ContractTemplateServiceImpl.java
@@ -14,6 +14,7 @@ import it.pagopa.selfcare.product.storage.ContractTemplateStorage;
 import it.pagopa.selfcare.product.util.HtmlUtils;
 import jakarta.enterprise.context.ApplicationScoped;
 import lombok.extern.slf4j.Slf4j;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.jboss.resteasy.reactive.multipart.FileUpload;
 
 import java.io.IOException;
@@ -31,18 +32,23 @@ public class ContractTemplateServiceImpl implements ContractTemplateService {
     private static final String BASE_TEMPLATE_FILE = "contract-template.html";
     private static final String BASE_TEMPLATE_FRAGMENT_PLACEHOLDER = "${contractTemplateFragment}";
     private static final String BASE_TEMPLATE_LOGO_PLACEHOLDER = "${contractTemplateLogo}";
+    private static final String BASE_TEMPLATE_LOGO_PATH = "%s/%s/logo.png"; // {contractTemplateLogoBaseUrl}/{productId}/logo.png
 
     private final ContractTemplateRepository contractTemplateRepository;
     private final ContractTemplateStorage contractTemplateStorage;
     private final ContractTemplateMapper contractTemplateMapper;
+    private final String contractTemplateLogoBaseUrl;
     private final String contractTemplateBaseHTML;
 
     public ContractTemplateServiceImpl(ContractTemplateRepository contractTemplateRepository,
                                        ContractTemplateStorage contractTemplateStorage,
-                                       ContractTemplateMapper contractTemplateMapper) throws IOException {
+                                       ContractTemplateMapper contractTemplateMapper,
+                                       @ConfigProperty(name = "product-ms.contract-template.logo-base-url")
+                                       String contractTemplateLogoBaseUrl) throws IOException {
         this.contractTemplateRepository = contractTemplateRepository;
         this.contractTemplateStorage = contractTemplateStorage;
         this.contractTemplateMapper = contractTemplateMapper;
+        this.contractTemplateLogoBaseUrl = contractTemplateLogoBaseUrl;
         this.contractTemplateBaseHTML = Files.readString(Path.of(
             Objects.requireNonNull(getClass().getClassLoader().getResource(BASE_TEMPLATE_FILE)).getPath()
         ));
@@ -51,7 +57,7 @@ public class ContractTemplateServiceImpl implements ContractTemplateService {
     @Override
     public Uni<ContractTemplateResponse> upload(ContractTemplateUploadRequest request) {
         final ContractTemplate contractTemplate = contractTemplateMapper.toContractTemplate(request);
-        final ContractTemplateFile contractTemplateFile = buildContractTemplateFile(request.getFile());
+        final ContractTemplateFile contractTemplateFile = buildContractTemplateFile(contractTemplate.getProductId(), request.getFile());
         contractTemplate.setFileType(contractTemplateFile.getType());
         return contractTemplateRepository.countWithFilters(request.getProductId(), request.getName(), request.getVersion())
                 .onItem().transformToUni(count -> {
@@ -87,9 +93,9 @@ public class ContractTemplateServiceImpl implements ContractTemplateService {
                         .toList());
     }
 
-    private ContractTemplateFile buildContractTemplateFile(FileUpload fileUpload) {
+    private ContractTemplateFile buildContractTemplateFile(String productId, FileUpload fileUpload) {
         if (fileUpload.contentType().equals(ContractTemplateFileType.HTML.getContentType())) {
-            return buildContractTemplateFileFromHTML(fileUpload);
+            return buildContractTemplateFileFromHTML(productId, fileUpload);
         } else if (fileUpload.contentType().equals(ContractTemplateFileType.PDF.getContentType())) {
             return ContractTemplateFile.builder().file(fileUpload.uploadedFile().toFile())
                     .type(ContractTemplateFileType.PDF).build();
@@ -98,10 +104,10 @@ public class ContractTemplateServiceImpl implements ContractTemplateService {
         }
     }
 
-    private ContractTemplateFile buildContractTemplateFileFromHTML(FileUpload fileUpload) {
+    private ContractTemplateFile buildContractTemplateFileFromHTML(String productId, FileUpload fileUpload) {
         return Optional.ofNullable(HtmlUtils.getCleanHTML(fileUpload.uploadedFile().toFile())).map(htmlFragment -> {
             String html = contractTemplateBaseHTML.replace(BASE_TEMPLATE_FRAGMENT_PLACEHOLDER, htmlFragment);
-            html = html.replace(BASE_TEMPLATE_LOGO_PLACEHOLDER, ""); // TODO
+            html = html.replace(BASE_TEMPLATE_LOGO_PLACEHOLDER, String.format(BASE_TEMPLATE_LOGO_PATH, contractTemplateLogoBaseUrl, productId));
             return ContractTemplateFile.builder()
                     .data(html.getBytes(StandardCharsets.UTF_8))
                     .type(ContractTemplateFileType.HTML)
