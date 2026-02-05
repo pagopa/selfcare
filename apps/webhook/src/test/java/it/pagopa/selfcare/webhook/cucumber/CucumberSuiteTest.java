cbf894a7acf6117037c4c6b6f09ca8e3e4be808e
andrea-putzu <andrea.putzu@pagopa.it>
2026-02-05 16:28:30 +0100

    [SELC8215] chore: Removing comments on composeContainer

--- apps/webhook/src/test/java/it/pagopa/selfcare/webhook/cucumber/CucumberSuiteTest.java
+++ apps/webhook/src/test/java/it/pagopa/selfcare/webhook/cucumber/CucumberSuiteTest.java
@@ -6,12 +6,15 @@ import io.quarkiverse.cucumber.CucumberQuarkusTest;
 import io.quarkus.test.junit.TestProfile;
 import io.restassured.RestAssured;
 import it.pagopa.selfcare.webhook.cucumber.config.IntegrationProfile;
+import java.io.File;
 import java.io.IOException;
+import java.time.Duration;
 import lombok.extern.slf4j.Slf4j;
 import org.eclipse.microprofile.config.ConfigProvider;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.testcontainers.containers.ComposeContainer;
+import org.testcontainers.containers.wait.strategy.Wait;
 
 @Slf4j
 @TestProfile(CucumberTestProfile.class)
@@ -19,11 +22,12 @@ import org.testcontainers.containers.ComposeContainer;
     features = "src/test/resources/features",
     glue = {"it.pagopa.selfcare.webhook.cucumber.steps", "it.pagopa.selfcare.webhook.util"},
     plugin = {
-      "pretty",
-      "html:target/cucumber-report/cucumber.html",
-      "json:target/cucumber-report/cucumber.json"
+        "pretty",
+        "html:target/cucumber-report/cucumber.html",
+        "json:target/cucumber-report/cucumber.json"
     })
 public class CucumberSuiteTest extends CucumberQuarkusTest {
+
   static MongoDatabase mongoDatabase;
   private static ComposeContainer composeContainer;
   public static String tokenTest;
@@ -40,13 +44,13 @@ public class CucumberSuiteTest extends CucumberQuarkusTest {
     RestAssured.port = 8081;
     tokenTest = ConfigProvider.getConfig().getValue(JWT_BEARER_TOKEN_ENV, String.class);
     log.info("Starting test containers...");
-    //    composeContainer = new ComposeContainer(new File("docker-compose.yml"))
-    //      .withLocalCompose(true).withPull(true)
-    //      .waitingFor("mongodb", Wait.forListeningPort())
-    //      .withStartupTimeout(Duration.ofMinutes(5));
-    //
-    //    composeContainer.start();
-    //    Runtime.getRuntime().addShutdownHook(new Thread(composeContainer::stop));
+    composeContainer = new ComposeContainer(new File("docker-compose.yml"))
+        .withLocalCompose(true).withPull(true)
+        .waitingFor("mongodb", Wait.forListeningPort())
+        .withStartupTimeout(Duration.ofMinutes(5));
+
+    composeContainer.start();
+    Runtime.getRuntime().addShutdownHook(new Thread(composeContainer::stop));
     log.info("Test containers started successfully");
     log.info(
         "\nLANGUAGE: {}\nCOUNTRY: {}\nTIMEZONE: {}\n",
