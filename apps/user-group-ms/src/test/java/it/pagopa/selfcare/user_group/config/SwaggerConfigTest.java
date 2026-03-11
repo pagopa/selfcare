0e4cacef9f42a96b357a884e8482b018f8b44118
AleDore <alessio85.dore@gmail.com>
2026-03-11 12:29:50 +0100

    fix: explicitly set swaggerEN profile in SwaggerConfigTest
    - Test was loading swaggerEN profile from application.yml anyway
    - Make it explicit with @ActiveProfiles annotation
    - TestPropertySource should now properly override MongoDB config

--- apps/user-group-ms/src/test/java/it/pagopa/selfcare/user_group/config/SwaggerConfigTest.java
+++ apps/user-group-ms/src/test/java/it/pagopa/selfcare/user_group/config/SwaggerConfigTest.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -23,6 +24,7 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
+@ActiveProfiles("swaggerEN")
 @TestPropertySource(properties = {
     "spring.data.mongodb.uri=mongodb://localhost:27017",
     "spring.data.mongodb.database=selcUserGroup"
