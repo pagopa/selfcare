63891fbc98e939ab6ea1a50a663d6134ab95cbd9
AleDore <alessio85.dore@gmail.com>
2026-03-11 14:44:45 +0100

    fix: use CDC workflow for user-group-ms to install libssl for embedded MongoDB
    The SwaggerConfigTest requires embedded MongoDB which needs libssl1.1 on ubuntu-24.04.
    Updated the user-group-ms workflow to use call_code_review_cdc.yml which includes libssl installation.
    Reverted SwaggerConfigTest to match original implementation without MongoDB exclusions.

--- apps/user-group-ms/src/test/java/it/pagopa/selfcare/user_group/config/SwaggerConfigTest.java
+++ apps/user-group-ms/src/test/java/it/pagopa/selfcare/user_group/config/SwaggerConfigTest.java
@@ -5,9 +5,6 @@ import it.pagopa.selfcare.user_group.model.mapper.UserGroupMapper;
 import it.pagopa.selfcare.user_group.service.UserGroupService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
-import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
@@ -26,7 +23,6 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
-@EnableAutoConfiguration(exclude = {MongoAutoConfiguration.class, MongoDataAutoConfiguration.class})
 @ActiveProfiles("swaggerEN")
 class SwaggerConfigTest {
 
