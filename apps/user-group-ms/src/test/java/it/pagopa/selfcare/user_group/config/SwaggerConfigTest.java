acea1e97ecc2af1ec68a091935e4e6651e610e4b
AleDore <alessio85.dore@gmail.com>
2026-03-11 15:07:42 +0100

    fix: remove swaggerEN profile from SwaggerConfigTest to use default test config
    - SwaggerConfigTest should use default test profile which has MongoDB config
    - Removed @ActiveProfiles("swaggerEN") annotation
    - Removed unused ActiveProfiles import
    - This aligns with the original repo's configuration

--- apps/user-group-ms/src/test/java/it/pagopa/selfcare/user_group/config/SwaggerConfigTest.java
+++ apps/user-group-ms/src/test/java/it/pagopa/selfcare/user_group/config/SwaggerConfigTest.java
@@ -8,7 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
@@ -23,7 +22,6 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
-@ActiveProfiles("swaggerEN")
 class SwaggerConfigTest {
 
     @MockBean
