75cf41d02f87bcc9d12b67a7e2609a1efea1bb0f
Giulia Tremolada <giulia.tremolada@nttdata.com>
2026-03-11 17:21:13 +0100

    fix existing unit tests

--- apps/auth/src/test/java/it/pagopa/selfcare/auth/util/OtpUtilsTest.java
+++ apps/auth/src/test/java/it/pagopa/selfcare/auth/util/OtpUtilsTest.java
@@ -28,47 +28,47 @@ public class OtpUtilsTest {
 
   @Test
   void completedAndSameIdp_ShouldNotRequireNewOtp() {
-    OtpFlow flow = createOtpFlow(OtpStatus.COMPLETED, OffsetDateTime.now().plusMinutes(5));
-    //Assertions.assertFalse(OtpUtils.isNewOtpFlowRequired(flow, true, 0));
+    OtpFlow flow = createOtpFlow(OtpStatus.COMPLETED, OffsetDateTime.now().plusMinutes(5), OffsetDateTime.now());
+    Assertions.assertFalse(OtpUtils.isNewOtpFlowRequired(flow, true, 0).await().indefinitely());
   }
 
   @Test
   void completedAndDifferentIdp_ShouldRequireNewOtp() {
-    OtpFlow flow = createOtpFlow(OtpStatus.COMPLETED, OffsetDateTime.now().plusMinutes(5));
-    //Assertions.assertTrue(OtpUtils.isNewOtpFlowRequired(flow, false, 0));
+    OtpFlow flow = createOtpFlow(OtpStatus.COMPLETED, OffsetDateTime.now().plusMinutes(5), OffsetDateTime.now());
+    Assertions.assertTrue(OtpUtils.isNewOtpFlowRequired(flow, false, 0).await().indefinitely());
   }
 
   @Test
   void expiredStatus_ShouldRequireNewOtp() {
-    OtpFlow flow = createOtpFlow(OtpStatus.EXPIRED, OffsetDateTime.now().plusMinutes(5));
-    //Assertions.assertTrue(OtpUtils.isNewOtpFlowRequired(flow, true, 0));
+    OtpFlow flow = createOtpFlow(OtpStatus.EXPIRED, OffsetDateTime.now().plusMinutes(5), OffsetDateTime.now());
+    Assertions.assertTrue(OtpUtils.isNewOtpFlowRequired(flow, true, 0).await().indefinitely());
   }
 
   @Test
   void rejectedStatus_ShouldRequireNewOtp() {
-    OtpFlow flow = createOtpFlow(OtpStatus.REJECTED, OffsetDateTime.now().plusMinutes(5));
-    //Assertions.assertTrue(OtpUtils.isNewOtpFlowRequired(flow, true, 0));
+    OtpFlow flow = createOtpFlow(OtpStatus.REJECTED, OffsetDateTime.now().plusMinutes(5), OffsetDateTime.now());
+    Assertions.assertTrue(OtpUtils.isNewOtpFlowRequired(flow, true, 0).await().indefinitely());
   }
 
   @Test
   void pendingAndNotExpired_ShouldNotRequireNewOtp() {
-    OtpFlow flow = createOtpFlow(OtpStatus.PENDING, OffsetDateTime.now().plusMinutes(5));
-    //Assertions.assertFalse(OtpUtils.isNewOtpFlowRequired(flow, true, 0));
+    OtpFlow flow = createOtpFlow(OtpStatus.PENDING, OffsetDateTime.now().plusMinutes(5), OffsetDateTime.now());
+    Assertions.assertFalse(OtpUtils.isNewOtpFlowRequired(flow, true, 0).await().indefinitely());
   }
 
   @Test
   void pendingAndExpired_ShouldRequireNewOtp() {
-    OtpFlow flow = createOtpFlow(OtpStatus.PENDING, OffsetDateTime.now().minusMinutes(1));
-    //Assertions.assertTrue(OtpUtils.isNewOtpFlowRequired(flow, true, 0));
+    OtpFlow flow = createOtpFlow(OtpStatus.PENDING, OffsetDateTime.now().minusMinutes(1), OffsetDateTime.now());
+    Assertions.assertTrue(OtpUtils.isNewOtpFlowRequired(flow, true, 0).await().indefinitely());
   }
 
   @Test
   void completedAndExpired_ShouldNotRequireNewOtp() {
-    OtpFlow flow = createOtpFlow(OtpStatus.COMPLETED, OffsetDateTime.now().minusMinutes(1));
-    //Assertions.assertFalse(OtpUtils.isNewOtpFlowRequired(flow, true, 0));
+    OtpFlow flow = createOtpFlow(OtpStatus.COMPLETED, OffsetDateTime.now().minusMinutes(1), OffsetDateTime.now());
+    Assertions.assertFalse(OtpUtils.isNewOtpFlowRequired(flow, true, 0).await().indefinitely());
   }
 
-  private OtpFlow createOtpFlow(OtpStatus status, OffsetDateTime expiresAt) {
-    return OtpFlow.builder().status(status).expiresAt(expiresAt).build();
+  private OtpFlow createOtpFlow(OtpStatus status, OffsetDateTime expiresAt, OffsetDateTime createdAt) {
+    return OtpFlow.builder().status(status).expiresAt(expiresAt).createdAt(createdAt).build();
   }
 }
