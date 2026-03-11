6383c0935b6a3b86d1e65a314d9c28d3dfc860e4
AleDore <alessio85.dore@gmail.com>
2026-03-11 12:16:04 +0100

    fix: add TestPropertySource to SwaggerConfigTest for MongoDB configuration
    - SwaggerConfigTest needs MongoDB database name from test properties
    - Add @TestPropertySource annotation to load application-test.properties

--- apps/user-group-ms/src/test/java/it/pagopa/selfcare/user_group/config/SwaggerConfigTest.java
+++ apps/user-group-ms/src/test/java/it/pagopa/selfcare/user_group/config/SwaggerConfigTest.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
@@ -22,6 +23,7 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
+@TestPropertySource(locations = "classpath:application-test.properties")
 class SwaggerConfigTest {
 
     @MockBean
