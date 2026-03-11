75870f02c9046340b3a4057a5677e00cad0e750b
AleDore <alessio85.dore@gmail.com>
2026-03-11 14:55:57 +0100

    fix: explicitly set MongoDB database name in SwaggerConfigTest
    Use @TestPropertySource to provide spring.data.mongodb.database property
    to avoid 'Database name must not be empty' error when running with swaggerEN profile.

--- apps/user-group-ms/src/test/java/it/pagopa/selfcare/user_group/config/SwaggerConfigTest.java
+++ apps/user-group-ms/src/test/java/it/pagopa/selfcare/user_group/config/SwaggerConfigTest.java
@@ -9,6 +9,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
@@ -23,7 +24,10 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
-@ActiveProfiles({"test", "swaggerEN"})
+@ActiveProfiles("swaggerEN")
+@TestPropertySource(properties = {
+    "spring.data.mongodb.database=selcUserGroup"
+})
 class SwaggerConfigTest {
 
     @MockBean
