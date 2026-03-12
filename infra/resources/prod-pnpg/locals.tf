77d79ac4c7cab8491649aac1758ac64bd4e69098
Giampiero Ferrara <giampiero.ferrara@emeal.nttdata.com>
2026-03-12 17:12:29 +0100

    Deleted storage_documents for PNPG

--- infra/resources/prod-pnpg/locals.tf
+++ infra/resources/prod-pnpg/locals.tf
@@ -26,19 +26,9 @@ locals {
     CostCenter  = "TS310 - PAGAMENTI & SERVIZI"
   }
 
-  cidr_subnet_document_storage = ["10.1.136.0/24"]
-
   key_vault_resource_group_name = "${local.prefix}-${local.env_short}-sec-rg"
   key_vault_name                = "${local.prefix}-${local.env_short}-kv"
 
   project                  = "${local.prefix}-${local.env_short}"
-  naming_config            = "documents"
   resource_group_name_vnet = "${local.project}-vnet-rg"
-
-  cidr_subnet_contract_storage = ["10.1.136.0/24"]
-
-  selc_documents_storage_connection_string = try(
-    data.azurerm_key_vault_secret.selc_documents_storage_connection_string.value,
-    ""
-  )
 }
