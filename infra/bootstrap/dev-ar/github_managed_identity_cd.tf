60bb149d71243f0632f36f362f5892fab3f32089
andrea-putzu <106688558+andrea-putzu@users.noreply.github.com>
2026-03-16 19:00:04 +0100

    chore(infra-bootstrap) Add managed identities (#162)

---
+++ infra/bootstrap/dev-ar/github_managed_identity_cd.tf
@@ -0,0 +1,141 @@
+module "identity_cd" {
+  source = "github.com/pagopa/terraform-azurerm-v4//github_federated_identity?ref=v9.4.0"
+
+  prefix    = local.prefix
+  env_short = local.env_short
+  domain    = local.domain
+
+  identity_role = "cd"
+
+  github_federations = local.cd_github_federations
+
+  cd_rbac_roles = {
+    subscription_roles = local.environment_cd_roles.subscription
+    resource_groups    = local.environment_cd_roles.resource_groups
+  }
+
+  tags = local.tags
+
+  depends_on = [
+    azurerm_resource_group.identity_rg
+  ]
+}
+
+module "identity_cd_ms" {
+  source = "github.com/pagopa/terraform-azurerm-v4//github_federated_identity?ref=v9.4.0"
+
+  prefix    = local.prefix
+  env_short = local.env_short
+  domain    = "ms"
+
+  identity_role = "cd"
+
+  github_federations = local.cd_github_federations_ms
+
+  cd_rbac_roles = {
+    subscription_roles = concat(local.environment_cd_roles_ms.subscription, ["${local.app} ${local.env} ContainerApp Jobs Writer"])
+    resource_groups    = local.environment_cd_roles_ms.resource_groups
+  }
+
+  tags = local.tags
+
+  depends_on = [
+    azurerm_resource_group.identity_rg,
+    azurerm_role_definition.container_apps_jobs_writer
+  ]
+}
+
+resource "azurerm_key_vault_access_policy" "key_vault_access_policy_identity_cd" {
+  key_vault_id = data.azurerm_key_vault.key_vault.id
+  tenant_id    = data.azurerm_client_config.current.tenant_id
+  object_id    = module.identity_cd_ms.identity_principal_id
+
+  secret_permissions = [
+    "Get",
+    "List",
+    "Set"
+  ]
+
+  certificate_permissions = [
+    "Get",
+    "List",
+  ]
+}
+
+resource "azurerm_key_vault_access_policy" "key_vault_access_policy_pnpg_identity_cd" {
+  key_vault_id = data.azurerm_key_vault.key_vault_pnpg.id
+  tenant_id    = data.azurerm_client_config.current.tenant_id
+  object_id    = module.identity_cd_ms.identity_principal_id
+
+  secret_permissions = [
+    "Get",
+    "List",
+    "Set"
+  ]
+}
+
+module "identity_cd_fe" {
+  source = "github.com/pagopa/terraform-azurerm-v4//github_federated_identity?ref=v9.4.0"
+
+  prefix    = local.prefix
+  env_short = local.env_short
+  domain    = "fe"
+
+  identity_role = "cd"
+
+  github_federations = local.cd_github_federations_fe
+
+  cd_rbac_roles = {
+    subscription_roles = local.environment_cd_roles_ms.subscription
+    resource_groups    = local.environment_cd_roles_ms.resource_groups
+  }
+
+  tags = local.tags
+
+  depends_on = [
+    azurerm_resource_group.identity_rg
+  ]
+}
+
+resource "azurerm_key_vault_access_policy" "key_vault_access_policy_identity_fe_cd" {
+  key_vault_id = data.azurerm_key_vault.key_vault.id
+  tenant_id    = data.azurerm_client_config.current.tenant_id
+  object_id    = module.identity_cd_fe.identity_principal_id
+
+  secret_permissions = [
+    "Get",
+    "List",
+  ]
+
+  certificate_permissions = [
+    "Get",
+    "List",
+  ]
+}
+
+resource "azurerm_key_vault_access_policy" "key_vault_access_policy_pnpg_identity_fe_cd" {
+  key_vault_id = data.azurerm_key_vault.key_vault_pnpg.id
+  tenant_id    = data.azurerm_client_config.current.tenant_id
+  object_id    = module.identity_cd_fe.identity_principal_id
+
+  secret_permissions = [
+    "Get",
+    "List",
+  ]
+}
+
+resource "azurerm_role_definition" "container_apps_jobs_writer" {
+  name        = "${local.app} ${local.env} ContainerApp Jobs Writer"
+  scope       = data.azurerm_subscription.current.id
+  description = "Custom role used to write container apps jobs execution properties"
+
+  permissions {
+    actions = [
+      "Microsoft.Authorization/roleDefinitions/write"
+    ]
+  }
+
+  assignable_scopes = [
+    data.azurerm_subscription.current.id
+  ]
+}
