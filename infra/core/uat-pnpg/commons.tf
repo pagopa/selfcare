ca6fd85619b408a63b84a1c6fbe44a0bcb4bd303
andrea-putzu <andrea.putzu@pagopa.it>
2026-03-20 16:09:03 +0100

    chore(infra): Update Network for CAE

--- infra/core/uat-pnpg/commons.tf
+++ infra/core/uat-pnpg/commons.tf
@@ -299,31 +299,31 @@ resource "null_resource" "cdn_storage_copy" {
 # # Container app environment
 # ###############################################################################
 
-# resource "azurerm_resource_group" "selc_container_app_rg" {
-#   name     = "${local.project}-container-app-rg"
-#   location = local.location
+resource "azurerm_resource_group" "selc_container_app_rg" {
+  name     = "${local.project}-container-app-rg"
+  location = local.location
 
-#   tags = local.tags
-# }
+  tags = local.tags
+}
 
-# module "networking" {
-#   source = "../_modules/networking"
+module "networking" {
+  source = "../_modules/networking"
 
-#   project = "${local.prefix}-${local.env_short}"
+  project = "${local.prefix}-${local.env_short}"
 
-#   core_vnet = false
+  core_vnet = false
 
-#   # inferred from vnet-common with cidr 10.1.0.0/16
-#   # https://github.com/pagopa/selfcare-infra/blob/9de7d03852904c1e743684a9edd40ae9df0645a8/src/core/01_network_0.tf#L9-L10
-#   cidr_subnet_cae  = "10.1.156.0/23"
-#   cidr_subnet_main = "10.1.148.0/23"
+  # inferred from vnet-common with cidr 10.1.0.0/16
+  # https://github.com/pagopa/selfcare-infra/blob/9de7d03852904c1e743684a9edd40ae9df0645a8/src/core/01_network_0.tf#L9-L10
+  cidr_subnet_cae  = "10.1.156.0/23"
+  cidr_subnet_main = "10.1.148.0/23"
 
-#   container_app_name_snet = "${local.project}-pnpg-cae-cp-snet"
+  container_app_name_snet = "${local.project}-pnpg-cae-001-snet"
 
-#   # delegation = []
+  delegation = []
 
-#   tags = local.tags
-# }
+  tags = local.tags
+}
 
 # module "container_app_environments" {
 #   source = "../_modules/container_app_environments"
