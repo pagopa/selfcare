bbcba386e070650bfbfea1abc8c557fb8cd2e116
andrea-putzu <andrea.putzu@pagopa.it>
2026-03-23 09:53:55 +0100

    chore(infra-core-prod): Initial setup

---
+++ infra/bootstrap/prod-ar/main.tf
@@ -0,0 +1,44 @@
+terraform {
+  required_version = ">=1.10.0"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.0"
+    }
+    github = {
+      source  = "integrations/github"
+      version = "~> 6.0"
+    }
+
+  }
+
+  backend "azurerm" {
+    # resource_group_name  = "io-infra-rg"
+    # storage_account_name = "selcustinfraterraform"
+    # container_name       = "azurermstate"
+    # key                  = "selc.infra.bootstrap.uat.tfstate"
+    resource_group_name  = "io-infra-rg"
+    storage_account_name = "selcpstinfraterraform"
+    container_name       = "terraform-state"
+    key                  = "selc.infra.bootstrap.prod.tfstate"
+    use_azuread_auth     = true
+  }
+}
+
+provider "azurerm" {
+  features {
+    key_vault {
+      purge_soft_delete_on_destroy = false
+    }
+  }
+  storage_use_azuread = true
+}
+
+provider "github" {
+  owner = "pagopa"
+}
+
+data "azurerm_subscription" "current" {}
+
+data "azurerm_client_config" "current" {}
