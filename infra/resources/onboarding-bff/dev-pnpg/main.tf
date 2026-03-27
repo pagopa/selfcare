256173281576788d5034018c899712f832a40cf6
Giampiero Ferrara <giampiero.ferrara@emeal.nttdata.com>
2026-03-27 15:06:27 +0100

    Renamed file

--- infra/resources/onboarding-bff/dev-pnpg/main.tf
+++ infra/resources/onboarding-bff/dev-pnpg/main.tf
@@ -1,11 +1,11 @@
 ###############################################################################
-# Onboarding Backend
+# Onboarding BFF
 ###############################################################################
 
 locals {
-  container_app_onboarding_backend = {
-    min_replicas = 1
-    max_replicas = 2
+  container_app_onboarding_bff = {
+    min_replicas = 0
+    max_replicas = 1
     scale_rules = [
       {
         custom = {
@@ -24,53 +24,53 @@ locals {
     memory = "1Gi"
   }
 
-  app_settings_onboarding_backend = [
+  app_settings_onboarding_bff = [
     { name = "APPLICATIONINSIGHTS_ROLE_NAME", value = "b4f-onboarding" },
     { name = "JAVA_TOOL_OPTIONS", value = "-javaagent:applicationinsights-agent.jar" },
     { name = "APPLICATIONINSIGHTS_INSTRUMENTATION_LOGGING_LEVEL", value = "OFF" },
     { name = "B4F_ONBOARDING_LOG_LEVEL", value = "DEBUG" },
     { name = "REST_CLIENT_LOGGER_LEVEL", value = "FULL" },
-    { name = "MS_ONBOARDING_URL", value = "http://selc-u-onboarding-ms-ca" },
-    { name = "MS_CORE_URL", value = "http://selc-u-ms-core-ca" },
-    { name = "USERVICE_PARTY_PROCESS_URL", value = "http://selc-u-ms-core-ca" },
-    { name = "USERVICE_PARTY_REGISTRY_PROXY_URL", value = "http://selc-u-party-reg-proxy-ca" },
+    { name = "MS_ONBOARDING_URL", value = "http://selc-d-pnpg-onboarding-ms-ca" },
+    { name = "MS_CORE_URL", value = "http://selc-d-pnpg-ms-core-ca" },
+    { name = "USERVICE_PARTY_PROCESS_URL", value = "http://selc-d-pnpg-ms-core-ca" },
+    { name = "USERVICE_PARTY_REGISTRY_PROXY_URL", value = "http://selc-d-pnpg-party-reg-proxy-ca" },
     { name = "USERVICE_USER_REGISTRY_URL", value = "https://api.uat.pdv.pagopa.it/user-registry/v1" },
     { name = "REST_CLIENT_CONNECT_TIMEOUT", value = "60000" },
     { name = "REST_CLIENT_READ_TIMEOUT", value = "60000" },
-    { name = "MS_USER_URL", value = "http://selc-u-user-ms-ca" },
-    { name = "PRODUCT_STORAGE_CONTAINER", value = "selc-u-product" },
-    { name = "ONBOARDING_FUNCTIONS_URL", value = "https://selc-u-onboarding-fn.azurewebsites.net" },
-    { name = "MS_USER_INSTITUTION_URL", value = "http://selc-u-user-ms-ca" },
-    { name = "MS_PRODUCT_URL", value = "http://selc-u-product-ms-ca" }
+    { name = "MS_USER_URL", value = "http://selc-d-pnpg-user-ms-ca" },
+    { name = "PRODUCT_STORAGE_CONTAINER", value = "selc-d-product" },
+    { name = "ONBOARDING_FUNCTIONS_URL", value = "https://selc-d-pnpg-onboarding-fn.azurewebsites.net" },
+    { name = "MS_USER_INSTITUTION_URL", value = "http://selc-d-user-ms-ca" },
+    { name = "MS_PRODUCT_URL", value = "http://selc-d-product-ms-ca" }
   ]
 
-  secrets_names_onboarding_backend = {
+  secrets_names_onboarding_bff = {
     "USERVICE_USER_REGISTRY_API_KEY"         = "user-registry-api-key"
     "APPLICATIONINSIGHTS_CONNECTION_STRING"  = "appinsights-connection-string"
     "JWT_TOKEN_PUBLIC_KEY"                   = "jwt-public-key"
     "BLOB_STORAGE_PRODUCT_CONNECTION_STRING" = "blob-storage-product-connection-string"
     "ONBOARDING-FUNCTIONS-API-KEY"           = "fn-onboarding-primary-key"
-    "USER-ALLOWED-LIST"                      = "user-allowed-list"
   }
 
-  private_dns_name_onboarding_backend = "selc-u-onboarding-backend-ca.mangopond-2a5d4d65.westeurope.azurecontainerapps.io"
-
-  apim_name = "selc-${local.env_short}-apim-v2"
-  apim_rg   = "selc-${local.env_short}-api-v2-rg"
+  private_dns_name_onboarding_bff    = "selc-d-pnpg-onboarding-bff-ca.blackhill-644148c0.westeurope.azurecontainerapps.io"
+  apim_name_onboarding_bff           = "selc-${local.env_short}-pnpg-apim-v2"
+  apim_rg_onboarding_bff             = "selc-${local.env_short}-pnpg-api-v2-rg"
+  dns_zone_prefix_onboarding_bff     = "pnpg.dev.selfcare"
+  api_dns_zone_prefix_onboarding_bff = "api-pnpg.dev.selfcare"
 }
 
-module "container_app_onboarding_backend" {
+module "container_app_onboarding_bff_pnpg" {
   source = "../../_modules/container_app_microservice"
 
   env_short                      = local.env_short
   resource_group_name            = local.ca_resource_group_name
-  container_app                  = local.container_app_onboarding_backend
-  container_app_name             = "selc-${local.env_short}-onboarding-backend"
+  container_app                  = local.container_app_onboarding_bff
+  container_app_name             = "selc-${local.env_short}-pnpg-onboarding-bff"
   container_app_environment_name = local.container_app_environment_name
-  image_name                     = "selfcare-onboarding-backend"
+  image_name                     = "selfcare-onboarding-bff"
   image_tag                      = local.onboarding_image_tag
-  app_settings                   = local.app_settings_onboarding_backend
-  secrets_names                  = local.secrets_names_onboarding_backend
+  app_settings                   = local.app_settings_onboarding_bff
+  secrets_names                  = local.secrets_names_onboarding_bff
   key_vault_resource_group_name  = local.key_vault_resource_group_name
   key_vault_name                 = local.key_vault_name
   probes                         = local.quarkus_health_probes
@@ -81,16 +81,16 @@ module "container_app_onboarding_backend" {
 # APIM
 ###############################################################################
 
-module "apim_api_bff_onboarding" {
+module "apim_api_bff_onboarding_pnpg" {
   source              = "../../_modules/apim_api"
-  apim_name           = local.apim_name
-  apim_rg             = local.apim_rg
-  api_name            = "selc-${local.env_short}-api-bff-onboarding"
-  display_name        = "BFF Onboarding API"
-  base_path           = "onboarding"
-  private_dns_name    = local.private_dns_name_onboarding_backend
-  dns_zone_prefix     = local.dns_zone_prefix
-  api_dns_zone_prefix = local.api_dns_zone_prefix
+  apim_name           = local.apim_name_onboarding_bff
+  apim_rg             = local.apim_rg_onboarding_bff
+  api_name            = "selc-${local.env_short}-pnpg-api-bff-onboarding"
+  display_name        = "BFF PNPG Onboarding API"
+  base_path           = "imprese/onboarding"
+  private_dns_name    = local.private_dns_name_onboarding_bff
+  dns_zone_prefix     = local.dns_zone_prefix_onboarding_bff
+  api_dns_zone_prefix = local.api_dns_zone_prefix_onboarding_bff
   external_domain     = "pagopa.it"
-  openapi_path        = "../../../apps/onboarding-backend/app/src/main/resources/swagger/api-docs.json"
+  openapi_path        = "../../../apps/onboarding-bff/app/src/main/resources/swagger/api-docs.json"
 }
--- infra/resources/onboarding-bff/prod-pnpg/main.tf
+++ infra/resources/onboarding-bff/prod-pnpg/main.tf
@@ -1,9 +1,9 @@
 ###############################################################################
-# Onboarding Backend
+# Onboarding BFF
 ###############################################################################
 
 locals {
-  container_app_onboarding_backend = {
+  container_app_onboarding_bff = {
     min_replicas = 1
     max_replicas = 5
     scale_rules = [
@@ -24,53 +24,53 @@ locals {
     memory = "2.5Gi"
   }
 
-  app_settings_onboarding_backend = [
+  app_settings_onboarding_bff = [
     { name = "APPLICATIONINSIGHTS_ROLE_NAME", value = "b4f-onboarding" },
     { name = "JAVA_TOOL_OPTIONS", value = "-javaagent:applicationinsights-agent.jar" },
     { name = "APPLICATIONINSIGHTS_INSTRUMENTATION_LOGGING_LEVEL", value = "OFF" },
     { name = "B4F_ONBOARDING_LOG_LEVEL", value = "INFO" },
     { name = "REST_CLIENT_LOGGER_LEVEL", value = "BASIC" },
-    { name = "MS_ONBOARDING_URL", value = "http://selc-p-onboarding-ms-ca" },
-    { name = "MS_CORE_URL", value = "http://selc-p-ms-core-ca" },
-    { name = "USERVICE_PARTY_PROCESS_URL", value = "http://selc-p-ms-core-ca" },
-    { name = "USERVICE_PARTY_REGISTRY_PROXY_URL", value = "http://selc-p-party-reg-proxy-ca" },
-    { name = "USERVICE_USER_REGISTRY_URL", value = "https://api.pdv.pagopa.it/user-registry/v1" },
-    { name = "REST_CLIENT_CONNECT_TIMEOUT", value = "60000" },
+    { name = "MS_ONBOARDING_URL", value = "http://selc-p-pnpg-onboarding-ms-ca" },
     { name = "REST_CLIENT_READ_TIMEOUT", value = "60000" },
-    { name = "MS_USER_URL", value = "http://selc-p-user-ms-ca" },
+    { name = "REST_CLIENT_CONNECT_TIMEOUT", value = "60000" },
+    { name = "MS_CORE_URL", value = "http://selc-p-pnpg-ms-core-ca" },
+    { name = "USERVICE_PARTY_PROCESS_URL", value = "http://selc-p-pnpg-ms-core-ca" },
+    { name = "USERVICE_PARTY_REGISTRY_PROXY_URL", value = "http://selc-p-pnpg-party-reg-proxy-ca" },
+    { name = "USERVICE_USER_REGISTRY_URL", value = "https://api.pdv.pagopa.it/user-registry/v1" },
+    { name = "MS_USER_URL", value = "http://selc-p-pnpg-user-ms-ca" },
     { name = "PRODUCT_STORAGE_CONTAINER", value = "selc-p-product" },
-    { name = "ONBOARDING_FUNCTIONS_URL", value = "https://selc-p-onboarding-fn.azurewebsites.net" },
-    { name = "MS_USER_INSTITUTION_URL", value = "http://selc-p-user-ms-ca" },
+    { name = "ONBOARDING_FUNCTIONS_URL", value = "https://selc-p-pnpg-onboarding-fn.azurewebsites.net" },
+    { name = "MS_USER_INSTITUTION_URL", value = "https://selc-p-user-ms-ca" },
     { name = "MS_PRODUCT_URL", value = "http://selc-p-product-ms-ca" }
   ]
 
-  secrets_names_onboarding_backend = {
+  secrets_names_onboarding_bff = {
     "USERVICE_USER_REGISTRY_API_KEY"         = "user-registry-api-key"
     "APPLICATIONINSIGHTS_CONNECTION_STRING"  = "appinsights-connection-string"
     "JWT_TOKEN_PUBLIC_KEY"                   = "jwt-public-key"
     "BLOB_STORAGE_PRODUCT_CONNECTION_STRING" = "blob-storage-product-connection-string"
     "ONBOARDING-FUNCTIONS-API-KEY"           = "fn-onboarding-primary-key"
-    "USER-ALLOWED-LIST"                      = "user-allowed-list"
   }
 
-  private_dns_name_onboarding_backend = "selc-p-onboarding-backend-ca.lemonpond-bb0b750e.westeurope.azurecontainerapps.io"
-
-  apim_name = "selc-${local.env_short}-apim-v2"
-  apim_rg   = "selc-${local.env_short}-api-v2-rg"
+  private_dns_name_onboarding_bff    = "selc-p-pnpg-onboarding-bff-ca.calmmoss-0be48755.westeurope.azurecontainerapps.io"
+  apim_name_onboarding_bff           = "selc-${local.env_short}-pnpg-apim-v2"
+  apim_rg_onboarding_bff             = "selc-${local.env_short}-pnpg-api-v2-rg"
+  dns_zone_prefix_onboarding_bff     = "imprese.notifichedigitali"
+  api_dns_zone_prefix_onboarding_bff = "api-pnpg.selfcare"
 }
 
-module "container_app_onboarding_backend" {
+module "container_app_onboarding_bff_pnpg" {
   source = "../../_modules/container_app_microservice"
 
   env_short                      = local.env_short
   resource_group_name            = local.ca_resource_group_name
-  container_app                  = local.container_app_onboarding_backend
-  container_app_name             = "selc-${local.env_short}-onboarding-backend"
+  container_app                  = local.container_app_onboarding_bff
+  container_app_name             = "selc-${local.env_short}-pnpg-onboarding-bff"
   container_app_environment_name = local.container_app_environment_name
-  image_name                     = "selfcare-onboarding-backend"
+  image_name                     = "selfcare-onboarding-bff"
   image_tag                      = local.onboarding_image_tag
-  app_settings                   = local.app_settings_onboarding_backend
-  secrets_names                  = local.secrets_names_onboarding_backend
+  app_settings                   = local.app_settings_onboarding_bff
+  secrets_names                  = local.secrets_names_onboarding_bff
   key_vault_resource_group_name  = local.key_vault_resource_group_name
   key_vault_name                 = local.key_vault_name
   probes                         = local.quarkus_health_probes
@@ -81,16 +81,16 @@ module "container_app_onboarding_backend" {
 # APIM
 ###############################################################################
 
-module "apim_api_bff_onboarding" {
+module "apim_api_bff_onboarding_pnpg" {
   source              = "../../_modules/apim_api"
-  apim_name           = local.apim_name
-  apim_rg             = local.apim_rg
-  api_name            = "selc-${local.env_short}-api-bff-onboarding"
-  display_name        = "BFF Onboarding API"
-  base_path           = "onboarding"
-  private_dns_name    = local.private_dns_name_onboarding_backend
-  dns_zone_prefix     = local.dns_zone_prefix
-  api_dns_zone_prefix = local.api_dns_zone_prefix
+  apim_name           = local.apim_name_onboarding_bff
+  apim_rg             = local.apim_rg_onboarding_bff
+  api_name            = "selc-${local.env_short}-pnpg-api-bff-onboarding"
+  display_name        = "BFF PNPG Onboarding API"
+  base_path           = "imprese/onboarding"
+  private_dns_name    = local.private_dns_name_onboarding_bff
+  dns_zone_prefix     = local.dns_zone_prefix_onboarding_bff
+  api_dns_zone_prefix = local.api_dns_zone_prefix_onboarding_bff
   external_domain     = "pagopa.it"
-  openapi_path        = "../../../apps/onboarding-backend/app/src/main/resources/swagger/api-docs.json"
+  openapi_path        = "../../../apps/onboarding-bff/app/src/main/resources/swagger/api-docs.json"
 }
