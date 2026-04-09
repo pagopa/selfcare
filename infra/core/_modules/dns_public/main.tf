0e9a1115e5384c5dc79ad0882d4a58eb4e2811a8
andrea-putzu <andrea.putzu@pagopa.it>
2026-04-09 12:07:54 +0200

    Update UAT DNS NS records: change nameservers to use new Azure DNS endpoints

--- infra/core/_modules/dns_public/main.tf
+++ infra/core/_modules/dns_public/main.tf
@@ -260,10 +260,10 @@ resource "azurerm_dns_ns_record" "uat_areariservata" {
   zone_name           = azurerm_dns_zone.areariservata_public[0].name
   resource_group_name = var.rg_vnet_name
   records = [
-    "ns1-34.azure-dns.com.",
-    "ns2-34.azure-dns.net.",
-    "ns3-34.azure-dns.org.",
-    "ns4-34.azure-dns.info.",
+    "ns1-33.azure-dns.com.",
+    "ns2-33.azure-dns.net.",
+    "ns3-33.azure-dns.org.",
+    "ns4-33.azure-dns.info.",
   ]
   ttl  = var.dns_default_ttl_sec
   tags = var.tags
