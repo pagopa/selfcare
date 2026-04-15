43118aa804d75c5c07abbbd99a5996610aeee4d0
andrea-putzu <andrea.putzu@pagopa.it>
2026-04-15 16:43:01 +0200

    Refactor workflows to use secrets for ARM tenant and subscription IDs

--- infra/core/_modules/cdn/main.tf
+++ infra/core/_modules/cdn/main.tf
@@ -85,7 +85,7 @@ data "azurerm_subnet" "cdn_snet" {
 ###############################################################################
 module "cdn_storage_account" {
   source  = "pagopa-dx/azure-storage-account/azurerm"
-  version = "~> 2.2"
+  version = "~> 2.1"
 
   environment = local.environment
 
