2dd609e8e32577bf6a2f819809afaf9b4ce699ef
andrea-putzu <andrea.putzu@pagopa.it>
2026-04-17 16:14:25 +0200

    fix: update workflow reference in pr_core_infra.yml to use local infra_plan.yml

--- infra/core/dev-pnpg/main.tf
+++ infra/core/dev-pnpg/main.tf
@@ -2,27 +2,11 @@ terraform {
   required_version = ">=1.10.0"
 
   required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 4.0"
-    }
-
-    azuread = {
-      source  = "hashicorp/azuread"
-      version = ">= 3.8.0"
-    }
-
-    random = {
-      source  = "hashicorp/random"
-      version = ">= 3.0.0"
-    }
-
-    pkcs12 = {
-      source  = "chilicat/pkcs12"
-      version = "0.0.7"
-    }
-    dx = { source = "pagopa-dx/azure", version = "~> 0.0" }
-
+    azurerm = { source = "hashicorp/azurerm", version = "~> 4.0" }
+    azuread = { source = "hashicorp/azuread", version = ">= 3.8.0" }
+    random  = { source = "hashicorp/random", version = ">= 3.0.0" }
+    pkcs12  = { source = "chilicat/pkcs12", version = "0.0.7" }
+    dx      = { source = "pagopa-dx/azure", version = "~> 0.0" }
   }
 
   backend "azurerm" {
