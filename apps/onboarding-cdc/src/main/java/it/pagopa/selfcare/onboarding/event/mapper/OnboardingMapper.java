3f5f386ae99e4c49ebd1dea90c3a387a1241b604
gianmarcoplutino <gianmarco.plutino@pagopa.it>
2026-04-17 17:15:10 +0200

    feat: added new RegistryIndexService

--- apps/onboarding-cdc/src/main/java/it/pagopa/selfcare/onboarding/event/mapper/OnboardingMapper.java
+++ apps/onboarding-cdc/src/main/java/it/pagopa/selfcare/onboarding/event/mapper/OnboardingMapper.java
@@ -2,11 +2,23 @@ package it.pagopa.selfcare.onboarding.event.mapper;
 
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.openapi.quarkus.onboarding_functions_json.model.Onboarding;
+import org.openapi.quarkus.party_registry_proxy_json.model.OnboardingIndexResource;
 
 import java.util.UUID;
 
 @Mapper(componentModel = "cdi", imports = UUID.class)
 public interface OnboardingMapper {
     Onboarding toEntity(it.pagopa.selfcare.onboarding.event.entity.Onboarding model);
+
+    @Mapping(target = "onboardingId", source = "id")
+    @Mapping(target = "institutionId", source = "institution.id")
+    @Mapping(target = "description", source = "institution.description")
+    @Mapping(target = "parentDescription", source = "institution.parentDescription")
+    @Mapping(target = "taxCode", source = "institution.taxCode")
+    @Mapping(target = "subunitCode", source = "institution.subunitCode")
+    @Mapping(target = "subunitType", source = "institution.subunitType")
+    @Mapping(target = "institutionType", source = "institution.institutionType")
+    OnboardingIndexResource toIndexResource(it.pagopa.selfcare.onboarding.event.entity.Onboarding model);
 }
