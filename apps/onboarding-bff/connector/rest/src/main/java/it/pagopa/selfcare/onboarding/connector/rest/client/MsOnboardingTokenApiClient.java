cc585b6c9488087b89c84e7659b05b514d71871d
giampiero-ferrara <giampiero-ferrara@users.noreply.github.com>
2026-04-17 15:08:26 +0000

    chore: update status page 87162ad815ca7f0a35afdb19dc6fe6837ed9e1ad

--- apps/onboarding-bff/connector/rest/src/main/java/it/pagopa/selfcare/onboarding/connector/rest/client/MsOnboardingTokenApiClient.java
+++
@@ -1,9 +0,0 @@
-package it.pagopa.selfcare.onboarding.connector.rest.client;
-
-import it.pagopa.selfcare.onboarding.generated.openapi.v1.api.TokenControllerApi;
-import org.springframework.cloud.openfeign.FeignClient;
-
-
-@FeignClient(name = "${rest-client.ms-onboarding-token-api.serviceCode}", url = "${rest-client.ms-onboarding.base-url}")
-public interface MsOnboardingTokenApiClient extends TokenControllerApi {
-}
