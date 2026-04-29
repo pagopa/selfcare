5f16fba30f35c23e50953af50cc13fa9df3bf40f
andrea-putzu <andrea.putzu@pagopa.it>
2026-04-29 15:07:47 +0200

    Remove obsolete OneTrust SDK stub file from the project

--- infra/core/prod-pnpg/app.tf
+++ infra/core/prod-pnpg/app.tf
@@ -225,17 +225,3 @@ resource "azurerm_nat_gateway" "nat_gateway" {
 #   checkout_cdn_storage_primary_access_key = module.cdn.storage_primary_access_key
 #   checkout_fe_rg_name                     = module.cdn.checkout_fe_rg_name
 # }
-
-###############################################################################
-# one trust
-###############################################################################
-
-module "one_trust" {
-  source = "../_modules/one_trust"
-
-  env                                     = local.env
-  checkout_cdn_name                       = module.cdn.storage_name
-  checkout_endpoint_name                  = module.cdn.name
-  checkout_cdn_storage_primary_access_key = module.cdn.storage_primary_access_key
-  checkout_fe_rg_name                     = module.cdn.checkout_fe_rg_name
-}
