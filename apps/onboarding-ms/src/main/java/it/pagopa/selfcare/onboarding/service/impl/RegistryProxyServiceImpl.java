b988c2eb1404771355d0bae2fc6916900515f5ff
andrea-putzu <andrea-putzu@users.noreply.github.com>
2026-05-07 15:35:48 +0000

    chore: update status page ed6b037d4518c8dcfe83a0d2119920dbc28dba36

--- apps/onboarding-ms/src/main/java/it/pagopa/selfcare/onboarding/service/impl/RegistryProxyServiceImpl.java
+++ apps/onboarding-ms/src/main/java/it/pagopa/selfcare/onboarding/service/impl/RegistryProxyServiceImpl.java
@@ -1,18 +1,18 @@
 package it.pagopa.selfcare.onboarding.service.impl;
 
 import io.smallrye.mutiny.Uni;
-import it.pagopa.selfcare.onboarding.service.ProxyRegistryService;
+import it.pagopa.selfcare.onboarding.service.RegistryProxyService;
 import jakarta.enterprise.context.ApplicationScoped;
 import org.eclipse.microprofile.rest.client.inject.RestClient;
 import org.openapi.quarkus.party_registry_proxy_json.api.UoApi;
 import org.openapi.quarkus.party_registry_proxy_json.model.UOResource;
 
 @ApplicationScoped
-public class ProxyRegistryServiceImpl implements ProxyRegistryService {
+public class RegistryProxyServiceImpl implements RegistryProxyService {
 
     private final UoApi uoApi;
 
-    public ProxyRegistryServiceImpl(@RestClient UoApi uoApi) {
+    public RegistryProxyServiceImpl(@RestClient UoApi uoApi) {
         this.uoApi = uoApi;
     }
 
--- apps/onboarding-ms/src/test/java/it/pagopa/selfcare/onboarding/service/RegistryProxyServiceImplTest.java
+++ apps/onboarding-ms/src/test/java/it/pagopa/selfcare/onboarding/service/RegistryProxyServiceImplTest.java
@@ -8,18 +8,18 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import io.smallrye.mutiny.Uni;
-import it.pagopa.selfcare.onboarding.service.impl.ProxyRegistryServiceImpl;
+import it.pagopa.selfcare.onboarding.service.impl.RegistryProxyServiceImpl;
 import org.junit.jupiter.api.Test;
 import org.openapi.quarkus.party_registry_proxy_json.api.UoApi;
 import org.openapi.quarkus.party_registry_proxy_json.model.UOResource;
 
-class ProxyRegistryServiceImplTest {
+class RegistryProxyServiceImplTest {
 
     @Test
     void findUoByRecipientCode_shouldDelegateToUoApi() {
         //given
         UoApi uoApi = mock(UoApi.class);
-        ProxyRegistryServiceImpl service = new ProxyRegistryServiceImpl(uoApi);
+        RegistryProxyServiceImpl service = new RegistryProxyServiceImpl(uoApi);
         UOResource expected = new UOResource();
         expected.setCodiceIpa("IPA001");
         Uni<UOResource> expectedUni = Uni.createFrom().item(expected);
@@ -31,5 +31,23 @@ class ProxyRegistryServiceImplTest {
         //then
         assertSame(expectedUni, result);
         verify(uoApi).findByUnicodeUsingGET1("recipient-001", null);
+    }
+
+    @Test
+    void findUoByRecipientCode_shouldDelegateToUoApiWithCategories() {
+        //given
+        UoApi uoApi = mock(UoApi.class);
+        RegistryProxyServiceImpl service = new RegistryProxyServiceImpl(uoApi);
+        UOResource expected = new UOResource();
+        expected.setCodiceIpa("IPA002");
+        Uni<UOResource> expectedUni = Uni.createFrom().item(expected);
+        when(uoApi.findByUnicodeUsingGET1(eq("recipient-002"), eq("PA,PSP"))).thenReturn(expectedUni);
+
+        //when
+        Uni<UOResource> result = service.findUoByRecipientCode("recipient-002", "PA,PSP");
+
+        //then
+        assertSame(expectedUni, result);
+        verify(uoApi).findByUnicodeUsingGET1("recipient-002", "PA,PSP");
     }
 }
