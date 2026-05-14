cd782b9fa4acc305f6bd2ef06cdff2f3fb66715c
Giampiero Ferrara <giampiero.ferrara@emeal.nttdata.com>
2026-05-14 09:22:29 +0200

    Added permission constants

---
+++ apps/onboarding-bff/web/src/main/java/it/pagopa/selfcare/onboarding/web/constants/PermissionConstants.java
@@ -0,0 +1,9 @@
+package it.pagopa.selfcare.onboarding.web.constants;
+
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = lombok.AccessLevel.PRIVATE)
+public final class PermissionConstants {
+    public static final String SELC_APPROVE_ACCOUNT_PAGE = "Selc:ApproveAccountPage";
+    public static final String SELC_VIEW_ACCOUNT_PAGE = "Selc:ViewAccountPage";
+}
