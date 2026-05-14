31028393d5014ae0a8672707240e0c4eff10ce05
Gaetano Miglionico <gaetano.miglionico@pagopa.it>
2026-05-14 10:48:15 +0200

    [SELC-8686] Fixed test

--- apps/registry-proxy/connector/rest/src/test/java/it/pagopa/selfcare/party/registry_proxy/connector/rest/SearchServiceConnectorImplTest.java
+++ apps/registry-proxy/connector/rest/src/test/java/it/pagopa/selfcare/party/registry_proxy/connector/rest/SearchServiceConnectorImplTest.java
@@ -395,11 +395,11 @@ public class SearchServiceConnectorImplTest {
 
   @Test
   void testSearchOnboarding() {
-    when(azureSearchRestClient.searchOnboarding("search", "all", "filter", true, 15L, 0L, null, "orderBy"))
+    when(azureSearchRestClient.searchOnboarding("\\\"search\\\"", "all", "filter", true, 15L, 0L, null, "orderBy"))
             .thenReturn(new SearchServiceIndexResponse<>());
     searchServiceConnector.searchOnboarding("search", "filter", 15L, 0L, "orderBy");
     verify(azureSearchRestClient, times(1))
-            .searchOnboarding("search", "all", "filter", true, 15L, 0L, null, "orderBy");
+            .searchOnboarding("\\\"search\\\"", "all", "filter", true, 15L, 0L, null, "orderBy");
   }
 
   @Test
