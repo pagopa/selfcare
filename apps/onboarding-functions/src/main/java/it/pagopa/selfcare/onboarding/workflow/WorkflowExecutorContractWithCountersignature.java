2fa8469e9a369846ec10c8745fc40f495e50f9b2
Giampiero Ferrara <giampiero.ferrara@emeal.nttdata.com>
2026-05-18 16:47:55 +0200

    Testing

--- apps/onboarding-functions/src/main/java/it/pagopa/selfcare/onboarding/workflow/WorkflowExecutorContractWithCountersignature.java
+++ apps/onboarding-functions/src/main/java/it/pagopa/selfcare/onboarding/workflow/WorkflowExecutorContractWithCountersignature.java
@@ -1,15 +1,15 @@
 package it.pagopa.selfcare.onboarding.workflow;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.microsoft.durabletask.TaskOptions;
 import com.microsoft.durabletask.TaskOrchestrationContext;
 import it.pagopa.selfcare.onboarding.common.OnboardingStatus;
 import it.pagopa.selfcare.onboarding.entity.Onboarding;
 import it.pagopa.selfcare.onboarding.entity.OnboardingWorkflow;
 import it.pagopa.selfcare.onboarding.entity.OnboardingWorkflowInstitution;
-import it.pagopa.selfcare.onboarding.mapper.OnboardingMapper;
 import it.pagopa.selfcare.onboarding.exception.GenericOnboardingException;
+import it.pagopa.selfcare.onboarding.mapper.OnboardingMapper;
 import it.pagopa.selfcare.product.entity.ManagingInstitution;
 import it.pagopa.selfcare.product.entity.SigningConfiguration;
 import lombok.extern.slf4j.Slf4j;
@@ -21,32 +21,20 @@ import java.util.Objects;
 import java.util.Optional;
 
 import static it.pagopa.selfcare.onboarding.entity.OnboardingWorkflowType.INSTITUTION;
-import static it.pagopa.selfcare.onboarding.functions.utils.ActivityName.BUILD_CONTRACT_ACTIVITY_NAME;
-import static it.pagopa.selfcare.onboarding.functions.utils.ActivityName.GET_LATEST_DOCUMENT_ACTIVITY;
-import static it.pagopa.selfcare.onboarding.functions.utils.ActivityName.GET_MANAGING_INSTITUTION_ACTIVITY;
-import static it.pagopa.selfcare.onboarding.functions.utils.ActivityName.GET_SIGNING_CONFIGURATION_ACTIVITY;
-import static it.pagopa.selfcare.onboarding.functions.utils.ActivityName.GET_USER_EMAIL_UUID_ACTIVITY;
-import static it.pagopa.selfcare.onboarding.functions.utils.ActivityName.SAVE_TOKEN_WITH_CONTRACT_ACTIVITY_NAME;
-import static it.pagopa.selfcare.onboarding.functions.utils.ActivityName.SEND_MAIL_NOTIFICATION_MANAGING_INSTITUTION;
-import static it.pagopa.selfcare.onboarding.functions.utils.ActivityName.SEND_MAIL_REGISTRATION_FOR_CONTRACT;
-import static it.pagopa.selfcare.onboarding.utils.Utils.getManagingInstitutionEmailRequestString;
-import static it.pagopa.selfcare.onboarding.utils.Utils.getManagingInstitutionSendEmailString;
-import static it.pagopa.selfcare.onboarding.utils.Utils.getOnboardingString;
-import static it.pagopa.selfcare.onboarding.utils.Utils.getOnboardingWorkflowString;
-import static it.pagopa.selfcare.onboarding.utils.Utils.readEmailList;
+import static it.pagopa.selfcare.onboarding.functions.utils.ActivityName.*;
+import static it.pagopa.selfcare.onboarding.utils.Utils.*;
 
 @Slf4j
 public class WorkflowExecutorContractWithCountersignature implements WorkflowExecutor {
 
+    private static final int DEFAULT_SIGNING_STEP = 1;
+    private static final int DEFAULT_REQUIRED_SIGNATURES = 1;
     private final ObjectMapper objectMapper;
     private final TaskOptions optionsRetry;
     private final OnboardingMapper onboardingMapper;
 
-    private static final int DEFAULT_SIGNING_STEP = 1;
-    private static final int DEFAULT_REQUIRED_SIGNATURES = 1;
 
-
-    public WorkflowExecutorContractWithCountersignature(ObjectMapper objectMapper, TaskOptions optionsRetry,  OnboardingMapper onboardingMapper) {
+    public WorkflowExecutorContractWithCountersignature(ObjectMapper objectMapper, TaskOptions optionsRetry, OnboardingMapper onboardingMapper) {
         this.objectMapper = objectMapper;
         this.optionsRetry = optionsRetry;
         this.onboardingMapper = onboardingMapper;
@@ -78,68 +66,75 @@ public class WorkflowExecutorContractWithCountersignature implements WorkflowExe
 
         DocumentResponse document = parseDocumentOrNull(latestDocumentString);
         int signingStep =
-            Objects.nonNull(document) && Objects.nonNull(document.getSigningStep())
-                ? document.getSigningStep()
-                : DEFAULT_SIGNING_STEP;
+                Objects.nonNull(document) && Objects.nonNull(document.getSigningStep())
+                        ? document.getSigningStep()
+                        : DEFAULT_SIGNING_STEP;
         int requiredSignatures =
-            Objects.nonNull(signingConfiguration)
-                ? signingConfiguration.getRequiredSignatures()
-                : DEFAULT_REQUIRED_SIGNATURES;
+                Objects.nonNull(signingConfiguration)
+                        ? signingConfiguration.getRequiredSignatures()
+                        : DEFAULT_REQUIRED_SIGNATURES;
 
         if (signingStep < requiredSignatures - 1) {
-        log.info(
-            "Contract with countersignature with onboardingId={} is still in {}, signingStep={}, requiredSignatures={}",
-            onboardingWorkflow.getOnboarding().getId(),
-            OnboardingStatus.PENDING,
-            signingStep,
-            requiredSignatures);
+            log.info(
+                    "Contract with countersignature with onboardingId={} is still in {}, signingStep={}, requiredSignatures={}",
+                    onboardingWorkflow.getOnboarding().getId(),
+                    OnboardingStatus.PENDING,
+                    signingStep,
+                    requiredSignatures);
             return Optional.empty();
         }
 
-        ManagingInstitution managingInstitution =
-            Collections.singletonList(
-                ctx.callActivity(GET_MANAGING_INSTITUTION_ACTIVITY,onboardingString,optionsRetry,ManagingInstitution.class)
-                    .await()).get(0);
+        ManagingInstitution[] managingInstitutionTest =
+                ctx.callActivity(GET_MANAGING_INSTITUTION_ACTIVITY, onboardingString, optionsRetry, ManagingInstitution[].class)
+                        .await();
+
+        log.info("Managing institution(s) found for onboardingId={}: {}", onboardingWorkflow.getOnboarding().getId(), managingInstitutionTest);
+
+        List<ManagingInstitution> managingInstitutions = managingInstitutionTest != null ? List.of(managingInstitutionTest) : Collections.emptyList();
+
+        log.info("Found {} managing institution(s) for onboardingId={}", managingInstitutions.size(), onboardingWorkflow.getOnboarding().getId());
+
+        ManagingInstitution managingInstitution = managingInstitutions.get(0);
 
         log.info(managingInstitution.getInstitutionId());
 
         String managingInstitutionEmailRequestString =
-            getManagingInstitutionEmailRequestString(
-                objectMapper,
-                managingInstitution.getInstitutionId(),
-                document.getProductId(),
-                onboardingWorkflow.getOnboarding().getId());
+                getManagingInstitutionEmailRequestString(
+                        objectMapper,
+                        managingInstitution.getInstitutionId(),
+                        document.getProductId(),
+                        onboardingWorkflow.getOnboarding().getId());
 
         log.info(managingInstitutionEmailRequestString);
 
         String emailsString =
-            ctx.callActivity(
-                    GET_USER_EMAIL_UUID_ACTIVITY,
-                    managingInstitutionEmailRequestString,
-                    optionsRetry,
-                    String.class)
-                .await();
+                ctx.callActivity(
+                                GET_USER_EMAIL_UUID_ACTIVITY,
+                                managingInstitutionEmailRequestString,
+                                optionsRetry,
+                                String.class)
+                        .await();
 
         List<String> emailsUuid = readEmailList(objectMapper, emailsString);
         log.info("Found {} email(s) for institution {} and product {}",
-            emailsUuid.size(),
-            managingInstitution.getInstitutionId(),
-            document.getProductId());
+                emailsUuid.size(),
+                managingInstitution.getInstitutionId(),
+                document.getProductId());
 
         emailsUuid.forEach(
-            emailUuid ->
-              ctx.callActivity(
-                      SEND_MAIL_NOTIFICATION_MANAGING_INSTITUTION,
-                      getManagingInstitutionSendEmailString(
-                          objectMapper,
-                          managingInstitution.getInstitutionId(),
-                          managingInstitution.getDescription(),
-                          document.getProductId(),
-                          emailUuid),
-                      optionsRetry,
-                      String.class)
-                  .await()
-            );
+                emailUuid ->
+                        ctx.callActivity(
+                                        SEND_MAIL_NOTIFICATION_MANAGING_INSTITUTION,
+                                        getManagingInstitutionSendEmailString(
+                                                objectMapper,
+                                                managingInstitution.getInstitutionId(),
+                                                managingInstitution.getDescription(),
+                                                document.getProductId(),
+                                                emailUuid),
+                                        optionsRetry,
+                                        String.class)
+                                .await()
+        );
 
         return Optional.of(OnboardingStatus.PENDING_IN_REVIEW);
     }
