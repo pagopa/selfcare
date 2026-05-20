7701ce176dc7809ebd29d456307af30488444299
Giampiero Ferrara <giampiero.ferrara@emeal.nttdata.com>
2026-05-20 11:21:40 +0200

    Added policy for document

--- infra/resources/_modules/apim_external_api/apim.tf
+++ infra/resources/_modules/apim_external_api/apim.tf
@@ -441,33 +441,26 @@ module "apim_external_api_ms_v2" {
     {
       operation_id = "deleteOnboarding"
       xml_content = templatefile("${path.module}/api/base_ms_url_external_product_onboarding_check_policy.xml.tpl", {
-        MS_BACKEND_URL         = "https://selc-${var.env_short}-onboarding-ms-ca.${var.ca_suffix_dns_private_name}/v1/"
-        TENANT_ID              = data.azurerm_client_config.current.tenant_id
-        EXTERNAL-OAUTH2-ISSUER = data.azurerm_key_vault_secret.external-oauth2-issuer.value
+        MS_BACKEND_URL = "https://selc-${var.env_short}-onboarding-ms-ca.${var.ca_suffix_dns_private_name}/v1/"
       })
     },
     {
       operation_id = "completeOnboardingUsingPUT"
       xml_content = templatefile("${path.module}/api/base_ms_url_external_product_onboarding_check_policy.xml.tpl", {
-        MS_BACKEND_URL         = "https://selc-${var.env_short}-onboarding-ms-ca.${var.ca_suffix_dns_private_name}/v1/"
-        TENANT_ID              = data.azurerm_client_config.current.tenant_id
-        EXTERNAL-OAUTH2-ISSUER = data.azurerm_key_vault_secret.external-oauth2-issuer.value
+        MS_BACKEND_URL = "https://selc-${var.env_short}-onboarding-ms-ca.${var.ca_suffix_dns_private_name}/v1/"
       })
     },
     {
       operation_id = "rejectOnboardingUsingPUT"
       xml_content = templatefile("${path.module}/api/base_ms_url_external_product_onboarding_check_policy.xml.tpl", {
-        MS_BACKEND_URL         = "https://selc-${var.env_short}-onboarding-ms-ca.${var.ca_suffix_dns_private_name}/v1/"
-        TENANT_ID              = data.azurerm_client_config.current.tenant_id
-        EXTERNAL-OAUTH2-ISSUER = data.azurerm_key_vault_secret.external-oauth2-issuer.value
+        MS_BACKEND_URL = "https://selc-${var.env_short}-onboarding-ms-ca.${var.ca_suffix_dns_private_name}/v1/"
       })
     },
     {
       operation_id = "getContractSigned"
-      xml_content = templatefile("${path.module}/api/base_ms_url_external_product_policy.xml.tpl", {
-        MS_BACKEND_URL         = "https://selc-${var.env_short}-document-ms-ca.${var.ca_suffix_dns_private_name}/v1/"
-        TENANT_ID              = data.azurerm_client_config.current.tenant_id
-        EXTERNAL-OAUTH2-ISSUER = data.azurerm_key_vault_secret.external-oauth2-issuer.value
+      xml_content = templatefile("${path.module}/api/base_ms_url_external_document_onboarding_check_policy.xml.tpl", {
+        MS_ONBOARDING_BE = "https://selc-${var.env_short}-onboarding-ms-ca.${var.ca_suffix_dns_private_name}/v1/"
+        MS_DOCUMENT_BE   = "https://selc-${var.env_short}-document-ms-ca.${var.ca_suffix_dns_private_name}/v1/"
       })
     }
   ]
