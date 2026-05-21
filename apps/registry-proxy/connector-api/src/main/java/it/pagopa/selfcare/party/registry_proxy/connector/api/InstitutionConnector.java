58b11a0d3e6795878195fd638caa15b480946e7a
Gaetano Miglionico <gaetano.miglionico@pagopa.it>
2026-05-21 10:13:37 +0200

    [SELC-8444] Using onboarding index in searchInstitutions API (#363)

--- apps/registry-proxy/connector-api/src/main/java/it/pagopa/selfcare/party/registry_proxy/connector/api/InstitutionConnector.java
+++
@@ -1,8 +0,0 @@
-package it.pagopa.selfcare.party.registry_proxy.connector.api;
-
-import it.pagopa.selfcare.party.registry_proxy.connector.model.institution.Institution;
-
-public interface InstitutionConnector {
-
-    Institution getById(String id);
-}
