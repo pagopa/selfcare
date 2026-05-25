817e4c8a1fd336f274cda7bf31e5faddb68b1e0a
andrea-putzu <andrea.putzu@pagopa.it>
2026-05-25 11:07:51 +0200

    feat: implement external scheduling for index updates and add main application entry point

--- infra/resources/registry-proxy-runner/dev-ar/main.tf
+++ infra/resources/registry-proxy-runner/dev-ar/main.tf
@@ -59,12 +59,6 @@ module "container_app" {
   key_vault_name                 = module.local.config.key_vault_name
   tags                           = module.local.config.tags
 
-  schedule_trigger_config = [{
-    cron_expression          = "0 */6 * * *"
-    parallelism              = 1
-    replica_completion_count = 1
-  }]
-
   manual_trigger_config = [{
     parallelism              = 1
     replica_completion_count = 1
