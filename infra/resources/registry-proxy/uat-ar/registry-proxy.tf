d123d7de1cba1593c1365fa66346642b7f9c8a5c
gaetano-miglionico <gaetano-miglionico@users.noreply.github.com>
2026-05-25 09:03:15 +0000

    chore: update status page 75cdf05abe7ef6daf8c22c905deedb813cdf8631

--- infra/resources/registry-proxy/uat-ar/registry-proxy.tf
+++ infra/resources/registry-proxy/uat-ar/registry-proxy.tf
@@ -237,10 +237,6 @@ locals {
       value = "https://selc-${module.local.config.env_short}-weu-ar-srch.search.windows.net/"
     },
     {
-      name  = "AZURE_SEARCH_INSTITUTION_INDEX"
-      value = "institution-index-ar"
-    },
-    {
       name  = "AZURE_SEARCH_IPA_INSTITUTION_INDEX"
       value = module.ai_search_ipa.institution_index_name
     },
