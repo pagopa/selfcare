5d48ccf0d05d4dc3ae87abaceee1dd5d47db72e9
giulia-tremolada <giulia-tremolada@users.noreply.github.com>
2026-05-27 15:24:26 +0000

    chore: update status page 458c336aaa08570a2183bed74045ae7b975d620a

--- infra/bootstrap/dev-ar/locals.tf
+++ infra/bootstrap/dev-ar/locals.tf
@@ -37,21 +37,13 @@ locals {
   }
 
   env_ci_secrets = {
-    "AZURE_CLIENT_ID_CI"    = module.identity_ci.identity_client_id
-    "AZURE_SUBSCRIPTION_ID" = data.azurerm_client_config.current.subscription_id
-    "AZURE_TENANT_ID"       = data.azurerm_client_config.current.tenant_id,
-    "ARM_CLIENT_ID_CI"      = module.identity_ci.identity_client_id
-    "ARM_SUBSCRIPTION_ID"   = data.azurerm_client_config.current.subscription_id
-    "ARM_TENANT_ID"         = data.azurerm_client_config.current.tenant_id,
+    "ARM_SUBSCRIPTION_ID" = data.azurerm_client_config.current.subscription_id
+    "ARM_TENANT_ID"       = data.azurerm_client_config.current.tenant_id,
   }
 
   env_cd_secrets = {
-    "AZURE_CLIENT_ID_CD"    = module.identity_cd.identity_client_id
-    "AZURE_SUBSCRIPTION_ID" = data.azurerm_client_config.current.subscription_id
-    "AZURE_TENANT_ID"       = data.azurerm_client_config.current.tenant_id,
-    "ARM_CLIENT_ID_CD"      = module.identity_cd.identity_client_id
-    "ARM_SUBSCRIPTION_ID"   = data.azurerm_client_config.current.subscription_id
-    "ARM_TENANT_ID"         = data.azurerm_client_config.current.tenant_id,
+    "ARM_SUBSCRIPTION_ID" = data.azurerm_client_config.current.subscription_id
+    "ARM_TENANT_ID"       = data.azurerm_client_config.current.tenant_id,
   }
 
   ci_github_federations = [
@@ -111,33 +103,6 @@ locals {
     }
   ]
 
-  ci_github_federations_ms = [
-    {
-      repository = "selfcare-dashboard-backend"
-      subject    = "${local.env}-ci"
-    },
-    {
-      repository = "selfcare-external-api-backend"
-      subject    = "${local.env}-ci"
-    },
-    {
-      repository = "selfcare-ms-core"
-      subject    = "${local.env}-ci"
-    },
-    {
-      repository = "selfcare-user"
-      subject    = "${local.env}-ci"
-    },
-    {
-      repository = "selfcare-institution"
-      subject    = "${local.env}-ci"
-    },
-    {
-      repository = "selfcare"
-      subject    = "${local.env}-ci"
-    }
-  ]
-
   cd_github_federations_fe = [
     {
       repository = "selfcare-assistance-frontend"
@@ -181,38 +146,13 @@ locals {
     }
   ]
 
-  cd_github_federations_ms = [
-    {
-      repository = "selfcare-dashboard-backend"
-      subject    = "${local.env}-cd"
-    },
-    {
-      repository = "selfcare-external-api-backend"
-      subject    = "${local.env}-cd"
-    },
-    {
-      repository = "selfcare-ms-core"
-      subject    = "${local.env}-cd"
-    },
-    {
-      repository = "selfcare-user"
-      subject    = "${local.env}-cd"
-    },
-    {
-      repository = "selfcare-institution"
-      subject    = "${local.env}-cd"
-    },
-    {
-      repository = "selfcare"
-      subject    = "${local.env}-cd"
-    }
-  ]
 
   environment_ci_roles = {
     subscription = [
       "Reader",
       "PagoPA IaC Reader",
-      "Reader and Data Access"
+      "Reader and Data Access",
+      "ContainerApp Reader"
     ]
     resource_groups = {
       "terraform-state-rg" = [
@@ -245,7 +185,16 @@ locals {
       ],
       "selc-${local.env_short}-weu-pnpg-cosmosdb-mongodb-rg" = [
         "DocumentDB Account Contributor"
-      ]
+      ],
+      "selc-${local.env_short}-contracts-storage-rg" = [
+        "Storage Blob Data Contributor"
+      ],
+      "selc-${local.env_short}-onboarding-fn-rg" = [
+        "Storage Account Key Operator Service Role"
+      ],
+      "selc-${local.env_short}-pnpg-onboarding-fn-rg" = [
+        "Storage Account Key Operator Service Role"
+      ],
     }
   }
 
@@ -254,6 +203,9 @@ locals {
       "Contributor"
     ]
     resource_groups = {
+      terraform-state-rg = [
+        "Storage Blob Data Contributor"
+      ],
       "selc-${local.env_short}-aks-rg" = [
         "Azure Kubernetes Service Cluster Admin Role"
       ],
@@ -277,26 +229,10 @@ locals {
 
   environment_ci_roles_ms = {
     subscription = [
-      "Reader",
-      "PagoPA IaC Reader",
-      "ContainerApp Reader"
+
     ]
     resource_groups = {
-      terraform-state-rg = [
-        "Storage Blob Data Contributor"
-      ],
-      "selc-${local.env_short}-contracts-storage-rg" = [
-        "Storage Blob Data Contributor"
-      ],
-      io-infra-rg = [
-        "Storage Blob Data Contributor"
-      ],
-      "selc-${local.env_short}-onboarding-fn-rg" = [
-        "Storage Account Key Operator Service Role"
-      ],
-      "selc-${local.env_short}-pnpg-onboarding-fn-rg" = [
-        "Storage Account Key Operator Service Role"
-      ],
+
     }
   }
 
@@ -305,12 +241,7 @@ locals {
       "Contributor"
     ]
     resource_groups = {
-      terraform-state-rg = [
-        "Storage Blob Data Contributor"
-      ],
-      io-infra-rg = [
-        "Storage Blob Data Contributor"
-      ],
+
     }
   }
 
@@ -339,16 +270,5 @@ locals {
     "selfcare-dashboard-users-microfrontend"  = "${local.env}"
     "selfcare-onboarding-frontend"            = "${local.env}"
     "selfcare-pnpg-dashboard-frontend"        = "${local.env}"
-  }
-  github_federations_ms = {
-    "selfcare"                         = "${local.env}"
-    "selfcare-dashboard-backend"       = "${local.env}"
-    "selfcare-external-api-backend"    = "${local.env}"
-    "selfcare-infra"                   = "${local.env}"
-    "selfcare-institution"             = "${local.env}"
-    "selfcare-ms-external-interceptor" = "${local.env}"
-    "selfcare-ms-party-registry-proxy" = "${local.env}"
-    "selfcare-onboarding-backend"      = "${local.env}"
-    "selfcare-user"                    = "${local.env}"
   }
 }
