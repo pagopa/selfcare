8c4c0cd38deb37fd62511af920eafff0632ba445
Giampiero Ferrara <giampiero.ferrara@emeal.nttdata.com>
2026-06-11 15:20:59 +0200

    Updated infra storage rbac permission

--- infra/resources/onboarding-ms/prod-ar/onboarding.tf
+++ infra/resources/onboarding-ms/prod-ar/onboarding.tf
@@ -114,6 +114,31 @@ resource "azurerm_key_vault_secret" "encryption_key_secret" {
   }
 }
 
+###############################################################################
+# DATA SOURCES
+###############################################################################
+data "azurerm_storage_account" "product_storage" {
+  name                = "selc${module.local.config.env_short}${module.local.config.location_short}archeckoutst01"
+  resource_group_name = "selc-${module.local.config.env_short}-checkout-fe-rg"
+}
+
+data "azurerm_user_assigned_identity" "cae_identity" {
+  name                = "${module.local.config.container_app_environment_name}-managed_identity"
+  resource_group_name = module.local.config.ca_resource_group_name
+}
+
+###############################################################################
+# RBAC
+###############################################################################
+resource "azurerm_role_assignment" "onboarding_ms_product_blob_contributor" {
+  scope                = data.azurerm_storage_account.product_storage.id
+  role_definition_name = "Storage Blob Data Contributor"
+  principal_id         = data.azurerm_user_assigned_identity.cae_identity.principal_id
+}
+
+###############################################################################
+# LOCAL VARIABLES
+###############################################################################
 locals {
   app_settings_onboarding_ms = [
     {
@@ -171,6 +196,14 @@ locals {
     {
       name  = "MS_PRODUCT_URL"
       value = "https://selc-${module.local.config.env_short}-product-ms-ca.${module.local.config.private_dns_name_domain}"
+    },
+    {
+      name  = "AZURE_STORAGE_ACCOUNT_NAME"
+      value = data.azurerm_storage_account.product_storage.name
+    },
+    {
+      name  = "AZURE_CLIENT_ID"
+      value = data.azurerm_user_assigned_identity.cae_identity.client_id
     }
   ]
 
@@ -180,13 +213,13 @@ locals {
     "MONGODB-CONNECTION-STRING"               = "mongodb-connection-string"
     "USER-REGISTRY-API-KEY"                   = "user-registry-api-key"
     "ONBOARDING-FUNCTIONS-API-KEY"            = "fn-onboarding-primary-key"
-    "BLOB-STORAGE-PRODUCT-CONNECTION-STRING"  = "blob-storage-product-connection-string"
     "APPLICATIONINSIGHTS_CONNECTION_STRING"   = "appinsights-connection-string"
     "ONBOARDING_DATA_ENCRIPTION_KEY"          = "onboarding-data-encryption-key"
     "ONBOARDING_DATA_ENCRIPTION_IV"           = "onboarding-data-encryption-iv"
     "NAMIRIAL_SIGN_SERVICE_IDENTITY_USER"     = "namirial-sign-service-user"
     "NAMIRIAL_SIGN_SERVICE_IDENTITY_PASSWORD" = "namirial-sign-service-psw"
   }
+
 }
 
 module "container_app_onboarding_ms" {
