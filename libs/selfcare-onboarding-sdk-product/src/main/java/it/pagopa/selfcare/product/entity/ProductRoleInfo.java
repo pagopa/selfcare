3b72f1a5a1df664c73da82a61a8781c58afc29ec
Giulia Tremolada <124147597+giulia-tremolada@users.noreply.github.com>
2026-06-11 14:55:35 +0200

    [SELC-8838] fix: adding equals and hasCode in ProductRoleInfo method (#452)

--- libs/selfcare-onboarding-sdk-product/src/main/java/it/pagopa/selfcare/product/entity/ProductRoleInfo.java
+++ libs/selfcare-onboarding-sdk-product/src/main/java/it/pagopa/selfcare/product/entity/ProductRoleInfo.java
@@ -1,6 +1,7 @@
 package it.pagopa.selfcare.product.entity;
 
 import java.util.List;
+import java.util.Objects;
 
 public class ProductRoleInfo {
 
@@ -33,6 +34,24 @@ public class ProductRoleInfo {
 
     public void setRoles(List<ProductRole> roles) {
         this.roles = roles;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof ProductRoleInfo that)) return false;
+        return Objects.equals(getPhasesAdditionAllowed(), that.getPhasesAdditionAllowed())
+                && Objects.equals(getRoles(), that.getRoles())
+                && Objects.equals(isSkipUserCreation(), that.isSkipUserCreation());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(
+                getPhasesAdditionAllowed(),
+                getRoles(),
+                isSkipUserCreation()
+        );
     }
 
 }
