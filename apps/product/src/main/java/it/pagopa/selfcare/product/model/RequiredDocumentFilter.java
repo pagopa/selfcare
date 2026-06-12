30cd18779028bde253b426a6d41390f603b05edd
gianmarcoplutino <gianmarco.plutino@pagopa.it>
2026-06-12 16:10:21 +0200

    feat: added requiredDocuments to Product

---
+++ apps/product/src/main/java/it/pagopa/selfcare/product/model/RequiredDocumentFilter.java
@@ -0,0 +1,18 @@
+package it.pagopa.selfcare.product.model;
+
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class RequiredDocumentFilter {
+
+  private List<String> institutionType;
+  private List<String> origin;
+}
+
