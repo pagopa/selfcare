86f36b5de8a14283a06251842375581b0c88c9a3
Giulia Tremolada <124147597+giulia-tremolada@users.noreply.github.com>
2026-06-15 17:18:03 +0200

    [SELC-8836] feat: modify model removing multiroleAllowed or multiroleGroups (#473)

--- apps/product-cdc/src/main/java/it/pagopa/selfcare/product/model/BackOfficeRole.java
+++ apps/product-cdc/src/main/java/it/pagopa/selfcare/product/model/BackOfficeRole.java
@@ -5,6 +5,8 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @Data
 @Builder
 @NoArgsConstructor
@@ -14,4 +16,5 @@ public class BackOfficeRole {
   private String label;
   private String description;
   private String productLabel;
+  private List<String> multiroleGroups;
 }
