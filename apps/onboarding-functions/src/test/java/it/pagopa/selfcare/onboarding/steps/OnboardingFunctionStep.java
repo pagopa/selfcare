ce10da0a1c90debbe5822464d5fae0fde4843314
Giampiero Ferrara <giampiero.ferrara@emeal.nttdata.com>
2026-06-22 10:23:38 +0200

    Fixed query param

--- apps/onboarding-functions/src/test/java/it/pagopa/selfcare/onboarding/steps/OnboardingFunctionStep.java
+++ apps/onboarding-functions/src/test/java/it/pagopa/selfcare/onboarding/steps/OnboardingFunctionStep.java
@@ -1,11 +1,5 @@
 package it.pagopa.selfcare.onboarding.steps;
 
-import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.allOf;
-import static org.hamcrest.Matchers.hasKey;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoDatabase;
@@ -22,9 +16,6 @@ import io.restassured.specification.RequestSpecification;
 import it.pagopa.selfcare.onboarding.entity.Institution;
 import it.pagopa.selfcare.onboarding.entity.Onboarding;
 import jakarta.inject.Inject;
-import java.util.List;
-import java.util.Map;
-import java.util.Objects;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.extern.slf4j.Slf4j;
@@ -34,6 +25,16 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.TestInstance.Lifecycle;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.hasKey;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @EqualsAndHashCode(callSuper = true)
 @CucumberOptions(
@@ -132,7 +133,7 @@ public class OnboardingFunctionStep extends CucumberQuarkusTest {
                         .log()
                         .all()
                         .queryParam("onboardingId", getOnboardingId())
-                        .queryParam("timeout", 1) //ToDo fix: APZ
+                        // .queryParam("timeout", 55000)
                         .when()
                         .get()
                         .then()
