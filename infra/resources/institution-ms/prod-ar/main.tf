b920b99dd15f4e3a753103820c0265236e1abe5a
Gaetano Miglionico <gaetano.miglionico@pagopa.it>
2026-06-23 13:07:17 +0200

    [SELC-8905] Fixed documents storage name

--- infra/resources/institution-ms/prod-ar/main.tf
+++ infra/resources/institution-ms/prod-ar/main.tf
@@ -33,7 +33,7 @@ data "azurerm_user_assigned_identity" "product_storage_blob_identity" {
 }
 
 data "azurerm_storage_account" "documents_storage" {
-  name                = "scd${module.local.config.env_short}${module.local.config.location_short}ardocumentsst01"
+  name                = "sc${module.local.config.env_short}${module.local.config.location_short}ardocumentsst01"
   resource_group_name = "selc-${module.local.config.env_short}-documents-fe-rg"
 }
 
