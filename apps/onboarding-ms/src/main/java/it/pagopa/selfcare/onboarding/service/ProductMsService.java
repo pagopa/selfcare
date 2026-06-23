a846eccf375d2177d73aca31731f3b5b19b483a3
Giampiero Ferrara <giampiero.ferrara@emeal.nttdata.com>
2026-06-23 16:49:00 +0200

    Added method for retrive documents

--- apps/onboarding-ms/src/main/java/it/pagopa/selfcare/onboarding/service/ProductMsService.java
+++ apps/onboarding-ms/src/main/java/it/pagopa/selfcare/onboarding/service/ProductMsService.java
@@ -1,11 +1,20 @@
 package it.pagopa.selfcare.onboarding.service;
 
 import io.smallrye.mutiny.Uni;
+import it.pagopa.selfcare.onboarding.common.ProductId;
+import jakarta.ws.rs.core.Response;
 import org.openapi.quarkus.product_json.model.InstitutionType;
 import org.openapi.quarkus.product_json.model.Origin;
+import org.openapi.quarkus.product_json.model.RequiredDocumentResponse;
 import org.openapi.quarkus.product_json.model.WorkflowTypeResponse;
 
+import java.util.List;
+
 public interface ProductMsService {
-    Uni<WorkflowTypeResponse> getWorkflowType(InstitutionType institutionType, Origin origin, String productId);
+  Uni<WorkflowTypeResponse> getWorkflowType(InstitutionType institutionType, Origin origin, ProductId productId);
+
+  Uni<List<RequiredDocumentResponse>> getRequiredDocuments(ProductId productId, InstitutionType institutionType, Origin origin);
+
+  Uni<Response> isRequiredDocumentsEnabled(ProductId productId, InstitutionType institutionType, Origin origin);
 }
 
--- apps/onboarding-ms/src/main/java/it/pagopa/selfcare/onboarding/service/impl/ProductMsServiceImpl.java
+++ apps/onboarding-ms/src/main/java/it/pagopa/selfcare/onboarding/service/impl/ProductMsServiceImpl.java
@@ -1,14 +1,21 @@
 package it.pagopa.selfcare.onboarding.service.impl;
 
 import io.smallrye.mutiny.Uni;
+import it.pagopa.selfcare.onboarding.common.ProductId;
 import it.pagopa.selfcare.onboarding.service.ProductMsService;
 import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.ws.rs.core.Response;
+import lombok.extern.slf4j.Slf4j;
 import org.eclipse.microprofile.rest.client.inject.RestClient;
 import org.openapi.quarkus.product_json.api.ProductApi;
 import org.openapi.quarkus.product_json.model.InstitutionType;
 import org.openapi.quarkus.product_json.model.Origin;
+import org.openapi.quarkus.product_json.model.RequiredDocumentResponse;
 import org.openapi.quarkus.product_json.model.WorkflowTypeResponse;
 
+import java.util.List;
+
+@Slf4j
 @ApplicationScoped
 public class ProductMsServiceImpl implements ProductMsService {
 
@@ -19,7 +26,20 @@ public class ProductMsServiceImpl implements ProductMsService {
     }
 
     @Override
-    public Uni<WorkflowTypeResponse> getWorkflowType(InstitutionType institutionType, Origin origin, String productId) {
-        return productController.getWorkflowType(institutionType, origin, productId);
+    public Uni<WorkflowTypeResponse> getWorkflowType(InstitutionType institutionType, Origin origin, ProductId productId) {
+        log.info("Calling getWorkflowType: productId={}, institutionType={}, origin={}", productId.getValue(), institutionType, origin);
+        return productController.getWorkflowType(institutionType, origin, productId.getValue());
+    }
+
+    @Override
+    public Uni<List<RequiredDocumentResponse>> getRequiredDocuments(ProductId productId, InstitutionType institutionType, Origin origin) {
+        log.info("Calling getRequiredDocuments: productId={}, institutionType={}, origin={}", productId.getValue(), institutionType, origin);
+        return productController.getRequiredDocuments(productId.getValue(), institutionType, origin);
+    }
+
+    @Override
+    public Uni<Response> isRequiredDocumentsEnabled(ProductId productId, InstitutionType institutionType, Origin origin) {
+        log.info("Calling isRequiredDocumentsEnabled: productId={}, institutionType={}, origin={}", productId.getValue(), institutionType, origin);
+        return productController.isRequiredDocumentsEnabled(productId.getValue(), institutionType, origin);
     }
 }
