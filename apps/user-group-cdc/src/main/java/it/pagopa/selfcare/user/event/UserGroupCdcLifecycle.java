32cacb981ee67a0b2048400c4bf6229ec160fc91
andrea-putzu <andrea-putzu@users.noreply.github.com>
2026-06-25 15:00:03 +0000

    chore: update status page 66164a724f7f939bff6db56800c646e152a2c229

--- apps/user-group-cdc/src/main/java/it/pagopa/selfcare/user/event/UserGroupCdcLifecycle.java
+++ apps/user-group-cdc/src/main/java/it/pagopa/selfcare/user/event/UserGroupCdcLifecycle.java
@@ -2,6 +2,7 @@ package it.pagopa.selfcare.user.event;
 
 import com.azure.data.tables.TableServiceClient;
 import com.azure.data.tables.TableServiceClientBuilder;
+import com.azure.identity.DefaultAzureCredentialBuilder;
 import io.quarkus.runtime.StartupEvent;
 import io.quarkus.runtime.configuration.ConfigUtils;
 import jakarta.enterprise.context.ApplicationScoped;
@@ -9,12 +10,15 @@ import jakarta.enterprise.event.Observes;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.Optional;
+
 @Slf4j
 @ApplicationScoped
 public class UserGroupCdcLifecycle {
-    @ConfigProperty(name = "user-group-cdc.storage.connection-string") String storageConnectionString;
+    @ConfigProperty(name = "user-group-cdc.storage.connection-string") Optional<String> storageConnectionString;
     @ConfigProperty(name = "user-group-cdc.table.name") String tableName;
-
+    @ConfigProperty(name = "user-group-cdc.storage-account-name") Optional<String> storageAccountName;
+    @ConfigProperty(name = "user-group-cdc.storage-client-id") Optional<String> managedIdentityClientId;
 
     void onStart(@Observes StartupEvent ev) {
 
@@ -26,9 +30,11 @@ public class UserGroupCdcLifecycle {
         log.info("The application is starting...");
 
         // Table CdCStartAt will be created
-        TableServiceClient tableServiceClient = new TableServiceClientBuilder()
-                .connectionString(storageConnectionString)
-                .buildClient();
+        final TableServiceClient tableServiceClient = storageConnectionString
+            .filter(cs -> !cs.isBlank())
+            .map(cs -> new TableServiceClientBuilder().connectionString(cs).buildClient())
+            .orElseGet(() -> new TableServiceClientBuilder().endpoint("https://" + storageAccountName.orElse("") + ".table.core.windows.net")
+                .credential(new DefaultAzureCredentialBuilder().managedIdentityClientId(managedIdentityClientId.orElse("")).build()).buildClient());
         tableServiceClient.createTableIfNotExists(tableName);
     }
 }
