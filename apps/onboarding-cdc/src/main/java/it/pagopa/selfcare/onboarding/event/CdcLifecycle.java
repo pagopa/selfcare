91b165028b7fcd5a52d998d1437379f9f6c1b1c5
Giampiero Ferrara <giampiero.ferrara@emeal.nttdata.com>
2026-06-25 14:21:49 +0200

    Fixed buildTableServiceClient

--- apps/onboarding-cdc/src/main/java/it/pagopa/selfcare/onboarding/event/CdcLifecycle.java
+++ apps/onboarding-cdc/src/main/java/it/pagopa/selfcare/onboarding/event/CdcLifecycle.java
@@ -17,33 +17,38 @@ import java.util.Optional;
 @ApplicationScoped
 public class CdcLifecycle {
 
-    @ConfigProperty(name = "onboarding-cdc.storage.connection-string") Optional<String> storageConnectionString;
-    @ConfigProperty(name = "onboarding-cdc.table.name") String tableName;
-    @ConfigProperty(name = "onboarding-cdc.storage-account-name") Optional<String> storageAccountName;
-    @ConfigProperty(name = "onboarding-cdc.managed-identity-client-id") Optional<String> managedIdentityClientId;
+  @ConfigProperty(name = "onboarding-cdc.storage.connection-string")
+  Optional<String> storageConnectionString;
+  @ConfigProperty(name = "onboarding-cdc.table.name")
+  String tableName;
+  @ConfigProperty(name = "onboarding-cdc.storage-account-name")
+  Optional<String> storageAccountName;
+  @ConfigProperty(name = "onboarding-cdc.managed-identity-client-id")
+  Optional<String> managedIdentityClientId;
 
 
-    void onStart(@Observes StartupEvent ev) {
+  void onStart(@Observes StartupEvent ev) {
 
-        if(ConfigUtils.getProfiles().contains("test")) {
-            //Not perform any action when testing
-            return;
-        }
-
-        log.info("The application is starting...");
-
-        // Table CdCStartAt will be created
-        final TableServiceClient tableServiceClient = storageConnectionString
-                .filter(cs -> !cs.isBlank())
-                .map(cs -> {
-                    log.info("Azure Table Storage: using connection string");
-                    return new TableServiceClientBuilder().connectionString(cs).buildClient();
-                })
-                .orElseGet(() -> {
-                    log.info("Azure Table Storage: using managed identity (clientId: {})", managedIdentityClientId.orElse("default"));
-                    return new TableServiceClientBuilder().endpoint("https://" + storageAccountName.orElse("") + ".table.core.windows.net")
-                            .credential(new DefaultAzureCredentialBuilder().managedIdentityClientId(managedIdentityClientId.orElse("")).build()).buildClient();
-                });
-        tableServiceClient.createTableIfNotExists(tableName);
+    if (ConfigUtils.getProfiles().contains("test")) {
+      //Not perform any action when testing
+      return;
     }
+
+    log.info("The application is starting...");
+    buildTableServiceClient().createTableIfNotExists(tableName);
+  }
+
+  TableServiceClient buildTableServiceClient() {
+    return storageConnectionString
+      .filter(cs -> !cs.isBlank())
+      .map(cs -> {
+        log.info("Azure Table Storage: using connection string");
+        return new TableServiceClientBuilder().connectionString(cs).buildClient();
+      })
+      .orElseGet(() -> {
+        log.info("Azure Table Storage: using managed identity (clientId: {})", managedIdentityClientId.orElse("default"));
+        return new TableServiceClientBuilder().endpoint("https://" + storageAccountName.orElse("") + ".table.core.windows.net")
+          .credential(new DefaultAzureCredentialBuilder().managedIdentityClientId(managedIdentityClientId.orElse("")).build()).buildClient();
+      });
+  }
 }
