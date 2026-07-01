6b0d5470d0b6ead43c8900270d134b24417c88c0
gianmarcoplutino <gianmarco.plutino@pagopa.it>
2026-07-01 14:56:25 +0200

    fix: updated cae name for uat pnpg

--- infra/resources/institution-ms/uat-pnpg/main.tf
+++ infra/resources/institution-ms/uat-pnpg/main.tf
@@ -12,7 +12,7 @@ module "local" {
   dns_zone_prefix                = "imprese.uat.notifichedigitali"
   api_dns_zone_prefix            = "api-pnpg.uat.selfcare"
   private_dns_name_domain        = "orangeground-0bd2d4dc.westeurope.azurecontainerapps.io"
-  container_app_environment_name = "selc-u-pnpg-cae-001"
+  container_app_environment_name = "selc-u-pnpg-cae-cp"
   ca_resource_group_name         = "selc-u-container-app-001-rg"
 }
 
