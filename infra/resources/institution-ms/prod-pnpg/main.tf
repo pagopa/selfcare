a46216ed58c56c51664e6302e53ad5b8b2665cb3
Giampiero Ferrara <giampiero.ferrara@emeal.nttdata.com>
2026-07-02 11:07:52 +0200

    Fixed managed identity name of storage

--- infra/resources/institution-ms/prod-pnpg/main.tf
+++ infra/resources/institution-ms/prod-pnpg/main.tf
@@ -25,8 +25,8 @@ module "local" {
 # DATA SOURCES
 ###############################################################################
 data "azurerm_storage_account" "product_storage" {
-  name                = "selc${module.local.config.env_short}${module.local.config.location_short}${module.local.config.domain}checkoutsa"
-  resource_group_name = "selc-${module.local.config.env_short}-${module.local.config.location_short}-${module.local.config.domain}-checkout-fe-rg"
+  name                = "selc${module.local.config.env_short}${module.local.config.location_short}pnpgcheckoutst01"
+  resource_group_name = "selc-${module.local.config.env_short}-${module.local.config.location_short}-pnpg-checkout-fe-rg"
 }
 
 data "azurerm_user_assigned_identity" "product_storage_blob_identity" {
