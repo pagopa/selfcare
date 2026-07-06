a087bb23922cfc269a02e7e47a6cb1f02572e55d
gaetano-miglionico <gaetano-miglionico@users.noreply.github.com>
2026-07-06 08:37:11 +0000

    chore: update status page dc2fa4a1233344bc0bb7e89ab63be30f0b9ad7f7

--- apps/onboarding-bff/connector/rest/src/main/java/it/pagopa/selfcare/onboarding/connector/rest/mapper/ProductMapper.java
+++ apps/onboarding-bff/connector/rest/src/main/java/it/pagopa/selfcare/onboarding/connector/rest/mapper/ProductMapper.java
@@ -1,11 +1,25 @@
 package it.pagopa.selfcare.onboarding.connector.rest.mapper;
 
 import it.pagopa.selfcare.onboarding.connector.model.product.OriginResult;
+import it.pagopa.selfcare.onboarding.connector.model.product.RequiredDocumentModel;
+import it.pagopa.selfcare.product.entity.StorageOrigin;
 import it.pagopa.selfcare.product.generated.openapi.v1.dto.ProductOriginResponse;
+import it.pagopa.selfcare.product.generated.openapi.v1.dto.RequiredDocumentResponse;
 import org.mapstruct.Mapper;
+
+import java.util.List;
+import java.util.Objects;
 
 @Mapper(componentModel = "spring", implementationName = "RestProductMapperImpl")
 public interface ProductMapper {
 
     OriginResult toOriginResult(ProductOriginResponse productOriginResponse);
+
+    RequiredDocumentModel toRequiredDocumentModel(RequiredDocumentResponse response);
+
+    List<RequiredDocumentModel> toRequiredDocumentModelList(List<RequiredDocumentResponse> responses);
+
+    default StorageOrigin toStorageOrigin(String value) {
+        return Objects.nonNull(value) ? StorageOrigin.valueOf(value) : null;
+    }
 }
