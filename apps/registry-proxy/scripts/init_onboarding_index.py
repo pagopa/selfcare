69715dbb11d42e4eaccf9b0d64558efdce4f3f8f
andrea-putzu <andrea-putzu@users.noreply.github.com>
2026-07-09 10:38:10 +0000

    chore: update status page 6c71ded979f500f97f3b9a1cc448ba6611aa0106

--- apps/registry-proxy/scripts/init_onboarding_index.py
+++ apps/registry-proxy/scripts/init_onboarding_index.py
@@ -69,6 +69,7 @@ def update_onboarding_index(onboardings: list[dict]):
                 **({"updatedAt": get_date_string(updatedAt)} if (updatedAt := o.get("updatedAt")) else {}),
                 **({"activatedAt": get_date_string(activatedAt)} if (activatedAt := o.get("activatedAt")) else {}),
                 **({"expiringDate": get_date_string(expiringDate)} if (expiringDate := o.get("expiringDate")) else {}),
+                **({"deletedAt": get_date_string(deletedAt)} if (deletedAt := o.get("deletedAt")) else {}),
                 **({"isTest": isTest} if (isTest := o.get("institution", {}).get("isTest")) else {}),
                 **({"city": city} if (city := o.get("institution", {}).get("city")) else {}),
                 **({"county": county} if (county := o.get("institution", {}).get("county")) else {}),
