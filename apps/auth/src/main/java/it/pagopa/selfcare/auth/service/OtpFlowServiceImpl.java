a51ba4d4be3e13579edf3a50e87e91f72f4a5de7
Giulia Tremolada <giulia.tremolada@nttdata.com>
2026-08-04 09:35:24 +0200

    [SELC-9081] fix: response email masked

--- apps/auth/src/main/java/it/pagopa/selfcare/auth/service/OtpFlowServiceImpl.java
+++ apps/auth/src/main/java/it/pagopa/selfcare/auth/service/OtpFlowServiceImpl.java
@@ -408,7 +408,7 @@ public class OtpFlowServiceImpl implements OtpFlowService {
       .map(emailStatus -> new OtpMailInfoResponse(
         emailStatus.getEmailId(),
         emailStatus.getStatus().toString(),
-        emailStatus.getTo().getEmail(),
+        OtpUtils.maskEmail(emailStatus.getTo().getEmail()),
         emailStatus.getAttempts(),
         emailStatus.getHistory().stream()
           .map(historyItem -> new OtpMailInfoResponse.MailStatusHistory(
