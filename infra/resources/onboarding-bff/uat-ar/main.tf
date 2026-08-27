5178bf100eb3231c185a83e25916aad337206e36
andrea-putzu <andrea.putzu@pagopa.it>
2026-08-27 14:08:06 +0200

    feat: configure tenant validation for onboarding
    - Read supported and default tenants from environment variables
    - Upgrade onboarding-bff to selfcare-commons 0.2.10
    - Propagate X-Tenant-Id to downstream services
    - Include the sanitized tenant in request and response logs

--- infra/resources/onboarding-bff/uat-ar/main.tf
+++ infra/resources/onboarding-bff/uat-ar/main.tf
@@ -74,16 +74,25 @@ module "container_app_onboarding_bff" {
 ###############################################################################
 
 module "apim_api_bff_onboarding" {
-  source              = "../../_modules/apim_api"
-  apim_name           = module.local.config.apim_name
-  apim_rg             = module.local.config.apim_rg
-  api_name            = "selc-${module.local.config.env_short}-api-bff-onboarding"
-  display_name        = "BFF Onboarding API"
-  base_path           = "onboarding"
-  private_dns_name    = "selc-${module.local.config.env_short}-onboarding-bff-ca.${module.local.config.private_dns_name_domain}"
-  dns_zone_prefix     = module.local.config.dns_zone_prefix
-  api_dns_zone_prefix = module.local.config.api_dns_zone_prefix
-  external_domain     = "pagopa.it"
-  openapi_path        = "../../../../apps/onboarding-bff/app/src/main/resources/swagger/api-docs.json"
-  tenant_ids          = module.local.config.tenant_ids
+  source                     = "../../_modules/apim_api"
+  apim_name                  = module.local.config.apim_name
+  apim_rg                    = module.local.config.apim_rg
+  api_name                   = "selc-${module.local.config.env_short}-api-bff-onboarding"
+  display_name               = "BFF Onboarding API"
+  base_path                  = "onboarding"
+  private_dns_name           = "selc-${module.local.config.env_short}-onboarding-bff-ca.${module.local.config.private_dns_name_domain}"
+  dns_zone_prefix            = module.local.config.dns_zone_prefix
+  api_dns_zone_prefix        = module.local.config.api_dns_zone_prefix
+  external_domain            = "pagopa.it"
+  openapi_path               = "../../../../apps/onboarding-bff/app/src/main/resources/swagger/api-docs.json"
+  tenant_ids                 = module.local.config.tenant_ids
+  tenant_hosts               = module.local.config.tenant_hosts
+  tenant_enforcement_enabled = true
+  allowed_headers = [
+    "Authorization",
+    "Content-Type",
+    "Accept",
+    "traceparent",
+    "tracestate"
+  ]
 }
