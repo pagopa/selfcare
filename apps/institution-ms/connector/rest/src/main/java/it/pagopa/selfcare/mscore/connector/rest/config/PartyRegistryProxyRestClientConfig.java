9faa6f5a501d2e01ba874484f1b18e3762926bcc
andrea-putzu <andrea.putzu@pagopa.it>
2026-08-28 12:14:37 +0200

    feat: configure tenant validation for institution-ms
    - Read supported and default tenants from environment variables
    - Upgrade institution-ms to selfcare-commons 2.10.0
    - Propagate X-Tenant-Id to downstream services
    - Include the sanitized tenant in request and response logs

--- apps/institution-ms/connector/rest/src/main/java/it/pagopa/selfcare/mscore/connector/rest/config/PartyRegistryProxyRestClientConfig.java
+++ apps/institution-ms/connector/rest/src/main/java/it/pagopa/selfcare/mscore/connector/rest/config/PartyRegistryProxyRestClientConfig.java
@@ -3,8 +3,10 @@ package it.pagopa.selfcare.mscore.connector.rest.config;
 import it.pagopa.selfcare.commons.connector.rest.config.RestClientBaseConfig;
 import it.pagopa.selfcare.commons.connector.rest.interceptor.AuthorizationHeaderInterceptor;
 import it.pagopa.selfcare.commons.connector.rest.interceptor.PartyTraceIdInterceptor;
+import it.pagopa.selfcare.mscore.connector.rest.interceptor.TenantHeaderInterceptor;
 import org.springframework.context.annotation.Import;
 
-@Import({RestClientBaseConfig.class, AuthorizationHeaderInterceptor.class, PartyTraceIdInterceptor.class})
+@Import({RestClientBaseConfig.class, AuthorizationHeaderInterceptor.class, PartyTraceIdInterceptor.class,
+        TenantHeaderInterceptor.class})
 public class PartyRegistryProxyRestClientConfig {
 }
