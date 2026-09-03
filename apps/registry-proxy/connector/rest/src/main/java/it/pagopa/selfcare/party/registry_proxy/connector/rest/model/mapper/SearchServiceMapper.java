4fa68f427483af5f908b70d784085270407aef93
Gaetano Miglionico <gaetano.miglionico@pagopa.it>
2026-09-03 17:22:13 +0200

    [SELC-9197] Remove onboardings with OVERRIDDEN status from index

--- apps/registry-proxy/connector/rest/src/main/java/it/pagopa/selfcare/party/registry_proxy/connector/rest/model/mapper/SearchServiceMapper.java
+++ apps/registry-proxy/connector/rest/src/main/java/it/pagopa/selfcare/party/registry_proxy/connector/rest/model/mapper/SearchServiceMapper.java
@@ -1,11 +1,6 @@
 package it.pagopa.selfcare.party.registry_proxy.connector.rest.model.mapper;
 
-import it.pagopa.selfcare.onboarding.common.OnboardingStatus;
-import it.pagopa.selfcare.party.registry_proxy.connector.model.IpaInstitution;
-import it.pagopa.selfcare.party.registry_proxy.connector.model.IpaInstitutionSearchResult;
-import it.pagopa.selfcare.party.registry_proxy.connector.model.OnboardingIndex;
-import it.pagopa.selfcare.party.registry_proxy.connector.model.OnboardingIndexSearch;
-import it.pagopa.selfcare.party.registry_proxy.connector.model.Origin;
+import it.pagopa.selfcare.party.registry_proxy.connector.model.*;
 import it.pagopa.selfcare.party.registry_proxy.connector.rest.model.search.IpaInstitutionIndex;
 import it.pagopa.selfcare.party.registry_proxy.connector.rest.model.search.SearchServiceIndexResponse;
 import it.pagopa.selfcare.party.registry_proxy.connector.rest.model.search.SearchServiceOnboardingIndex;
@@ -13,13 +8,14 @@ import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.Named;
 
-import java.time.OffsetDateTime;
-
 @Mapper(componentModel = "spring")
 public interface SearchServiceMapper {
 
     @Mapping(target = "action", constant = "mergeOrUpload")
     SearchServiceOnboardingIndex toSearchServiceOnboardingIndex(OnboardingIndex onboardingIndex);
+
+    @Mapping(target = "action", constant = "delete")
+    SearchServiceOnboardingIndex toDeletedSearchServiceOnboardingIndex(String onboardingId);
 
     @Mapping(target = "totalElements", source = "count")
     @Mapping(target = "onboardings", source = "value")
