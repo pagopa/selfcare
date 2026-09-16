5e0a9d3cc3528cd8c68a6c26e0558626c416e3e9
Gaetano Miglionico <gaetano.miglionico@pagopa.it>
2026-09-16 10:42:21 +0200

    [SELC-9275] dashboard-bff: health checks

--- apps/dashboard-bff/src/main/java/it/pagopa/selfcare/dashboard/config/BffDashboardConfig.java
+++ apps/dashboard-bff/src/main/java/it/pagopa/selfcare/dashboard/config/BffDashboardConfig.java
@@ -1,5 +1,6 @@
 package it.pagopa.selfcare.dashboard.config;
 
+import it.pagopa.selfcare.azurestorage.AzureBlobClientDefault;
 import it.pagopa.selfcare.product.service.ProductService;
 import it.pagopa.selfcare.product.service.ProductServiceCacheable;
 import lombok.RequiredArgsConstructor;
@@ -15,12 +16,21 @@ public class BffDashboardConfig {
     private final DashboardConfig config;
 
     @Bean
-    public ProductService productService(){
+    public ProductService productService(AzureBlobClientDefault productBlobClient) {
+        return new ProductServiceCacheable(
+                productBlobClient, config.getBlobStorage().getFilepathProduct());
+    }
+
+    @Bean
+    public AzureBlobClientDefault productBlobClient() {
         return Optional.ofNullable(config.getBlobStorage().getConnectionStringProduct())
             .filter(cs -> !cs.isBlank())
-            .map(cs -> new ProductServiceCacheable(cs, config.getBlobStorage().getContainerProduct(), config.getBlobStorage().getFilepathProduct()))
-            .orElseGet(() -> new ProductServiceCacheable(config.getBlobStorage().getContainerProduct(), config.getBlobStorage().getFilepathProduct(),
-                config.getBlobStorage().getAccountNameProduct(), config.getBlobStorage().getManagedIdentityClientIdProduct()));
+            .map(cs -> new AzureBlobClientDefault(
+                    cs, config.getBlobStorage().getContainerProduct()))
+            .orElseGet(() -> new AzureBlobClientDefault(
+                    config.getBlobStorage().getContainerProduct(),
+                    config.getBlobStorage().getAccountNameProduct(),
+                    config.getBlobStorage().getManagedIdentityClientIdProduct()));
     }
 
 }
